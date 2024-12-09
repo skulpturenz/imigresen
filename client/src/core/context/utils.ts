59c7f7bff4ed8733140c9053ffedbe0ac914b41d
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-09 07:13:31 +0000

    refactor: invariant

--- client/src/core/context/utils.ts
+++ client/src/core/context/utils.ts
@@ -1,12 +1,11 @@
+import { invariant } from "es-toolkit";
 import type { Context } from "solid-js";
 import { useContext as useSolidContext } from "solid-js";
 
 export const useContext = <T extends any>(context: Context<T>) => {
 	const value = useSolidContext(context);
 
-	if (!value) {
-		throw new Error("Context not defined");
-	}
+	invariant(value, "Context not defined");
 
 	return value;
 };
