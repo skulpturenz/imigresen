7f34491b2131bd91d10b933c2e486ff83925bf4e
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-11 06:57:06 +0000

    fix: update LoginCallback redirect

--- client/src/feat/auth/login-callback.tsx
+++ client/src/feat/auth/login-callback.tsx
@@ -1,4 +1,5 @@
 import { Navigate, useSearchParams } from "@solidjs/router";
+import { AuthRoute } from "core/constants/auth-route.enum";
 import { CoreRoute } from "core/constants/core-route.enum";
 import { toPath } from "core/router/route";
 import type { Component, ParentProps } from "solid-js";
@@ -6,7 +7,17 @@ import type { Component, ParentProps } from "solid-js";
 export const LoginCallback: Component<ParentProps> = () => {
 	const [searchParams] = useSearchParams<{ redirectPath: string }>();
 
-	return (
-		<Navigate href={searchParams.redirectPath ?? toPath(CoreRoute.Home)} />
-	);
+	const getRedirectPath = () => {
+		if (
+			searchParams.redirectPath &&
+			searchParams.redirectPath !==
+				toPath(CoreRoute.Auth, AuthRoute.LoginCallback)
+		) {
+			return searchParams.redirectPath;
+		}
+
+		return toPath(CoreRoute.Home);
+	};
+
+	return <Navigate href={getRedirectPath()} />;
 };
