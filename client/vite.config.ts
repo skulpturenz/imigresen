a3a78fbc447c8a2dbc7341657e67f6de731ce70a
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-11 09:31:24 +0000

    chore: add brotli compression

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,18 @@
 import { join } from "node:path";
 import { defineConfig } from "vite";
+import { default as viteCompression } from "vite-plugin-compression";
 import { default as solid } from "vite-plugin-solid";
 import { default as webfontDownload } from "vite-plugin-webfont-dl";
 
 export default defineConfig({
-	plugins: [solid(), webfontDownload()],
+	plugins: [
+		solid(),
+		webfontDownload(),
+		viteCompression({
+			verbose: true,
+			algorithm: "brotliCompress",
+		}),
+	],
 	resolve: {
 		alias: {
 			feat: join(__dirname, "./src/feat"),
