fa6ccfbb76114c95186800d4499490b14546cd7f
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-13 19:17:09 +0000

    feat: add button stories

--- client/src/ui/button/button.stories.tsx
+++ client/src/ui/button/button.stories.tsx
@@ -1,11 +1,82 @@
+import { X } from "lucide-solid";
 import type { Meta, StoryObj as Story } from "storybook-solidjs";
+import { Button } from "ui/button";
 
 export default {
 	title: "ui/button",
+	component: Button,
 	parameters: {
 		layout: "centered",
 	},
 	tags: ["autodocs"],
-} satisfies Meta;
+} satisfies Meta<typeof Button>;
 
-export const Primary: Story = {};
+export const DefaultVariant: Story<typeof Button> = {
+	args: {
+		variant: "default",
+		children: "Button",
+	},
+};
+
+export const DestructiveVariant: Story<typeof Button> = {
+	args: {
+		variant: "destructive",
+		children: "Button",
+	},
+};
+
+export const OutlineVariant: Story<typeof Button> = {
+	args: {
+		variant: "outline",
+		children: "Button",
+	},
+};
+
+export const SecondaryVariant: Story<typeof Button> = {
+	args: {
+		variant: "secondary",
+		children: "Button",
+	},
+};
+
+export const GhostVariant: Story<typeof Button> = {
+	args: {
+		variant: "ghost",
+		children: "Button",
+	},
+};
+
+export const LinkVariant: Story<typeof Button> = {
+	args: {
+		variant: "link",
+		children: "Button",
+	},
+};
+
+export const DefaultSize: Story<typeof Button> = {
+	args: {
+		size: "default",
+		children: "Button",
+	},
+};
+
+export const LargeSize: Story<typeof Button> = {
+	args: {
+		size: "lg",
+		children: "Button",
+	},
+};
+
+export const SmallSize: Story<typeof Button> = {
+	args: {
+		size: "sm",
+		children: "Button",
+	},
+};
+
+export const IconSize: Story<typeof Button> = {
+	args: {
+		size: "icon",
+		children: <X />,
+	},
+};
