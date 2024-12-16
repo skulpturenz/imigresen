e289a0c1491cdec1e34898090e353d20b3c656f4
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-16 05:01:03 +0000

    feat: add breadcrumb

---
+++ client/src/ui/breadcrumb/breadcrumb.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj as Story } from "storybook-solidjs";
+import {
+	Breadcrumb,
+	BreadcrumbEllipsis,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "ui/breadcrumb";
+
+export default {
+	title: "ui/breadcrumb",
+	parameters: {
+		docs: {
+			description: {
+				component:
+					"Displays the path to the current resource using a hierarchy of links",
+			},
+		},
+	},
+} satisfies Meta;
+
+export const Default: Story = {
+	render: () => (
+		<Breadcrumb>
+			<BreadcrumbList>
+				<BreadcrumbItem>
+					<BreadcrumbLink>Home</BreadcrumbLink>
+				</BreadcrumbItem>
+				<BreadcrumbSeparator />
+				<BreadcrumbItem>
+					<BreadcrumbEllipsis class="h-4 w-4" />
+					<span class="sr-only">Toggle menu</span>
+				</BreadcrumbItem>
+				<BreadcrumbSeparator />
+				<BreadcrumbItem>
+					<BreadcrumbLink>Components</BreadcrumbLink>
+				</BreadcrumbItem>
+				<BreadcrumbSeparator />
+				<BreadcrumbItem>
+					<BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+				</BreadcrumbItem>
+			</BreadcrumbList>
+		</Breadcrumb>
+	),
+};
