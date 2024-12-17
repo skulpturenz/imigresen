5dd1f861078098778b9a0962252d493eec297214
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-17 05:38:09 +0000

    chore: customize storybook theme

---
+++ client/.storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming";
+
+export const theme = create({
+	base: "dark",
+	brandTitle: "Imigresen",
+	fontBase: "Manrope, sans-serif",
+	colorPrimary: "hsl(47.9, 95.8%, 53.1%)",
+});
