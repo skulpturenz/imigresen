ef69d80a828cb5a3a421959d66183c3f3f32d49e
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-18 04:13:51 +0000

    chore: update sb dark theme colors for appBg and appContentBg

--- client/.storybook/theme.ts
+++ client/.storybook/theme.ts
@@ -5,4 +5,6 @@ export const darkTheme = create({
 	brandTitle: "Imigresen",
 	fontBase: "Manrope, sans-serif",
 	colorPrimary: "hsl(47.9, 95.8%, 53.1%)",
+	appBg: "hsl(20, 14.3%, 4.1%)",
+	appContentBg: "hsl(20, 14.3%, 4.1%)",
 });
