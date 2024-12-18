2a0fd06fd6d6aaeadb937976f64cbb12dc089a63
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-18 08:42:18 +0000

    test: add test for values which cannot be serialized

--- client/src/core/state/search-params-storage/search-params-storage.test.tsx
+++ client/src/core/state/search-params-storage/search-params-storage.test.tsx
@@ -5,7 +5,7 @@ import { createJSONStorage, persist } from "zustand/middleware";
 import { createSearchParamsStorage } from ".";
 
 describe("search-params-storage", () => {
-	it("persist state in search params", () => {
+	it("persists state in search params", () => {
 		const useStore = createWithSignal<Record<string, any>>(
 			persist(
 				(set, _get) => ({
@@ -37,6 +37,38 @@ describe("search-params-storage", () => {
 			a: "b",
 			"some-number": "1",
 			"has spaces": "2",
+		});
+	});
+
+	it("ignores values which cannot be serialized", () => {
+		const useStore = createWithSignal<Record<string, any>>(
+			persist(
+				(set, _get) => ({
+					hello: "world",
+					actions: {
+						addSearchParams: (key: string, value: string) =>
+							set({ [key]: value }),
+					},
+				}),
+				{
+					name: "test",
+					storage: createJSONStorage(createSearchParamsStorage),
+				},
+			) as any,
+		);
+
+		renderHook(() => {
+			const value = useStore();
+
+			value().actions.addSearchParams("a", () => {});
+			value().actions.addSearchParams("some-number", { hello: "world " });
+			value().actions.addSearchParams("has spaces", Symbol("test"));
+		});
+
+		const searchParams = new URLSearchParams(location.search.slice(1));
+
+		expect(Object.fromEntries(searchParams)).toEqual({
+			hello: "world",
 		});
 	});
 });
