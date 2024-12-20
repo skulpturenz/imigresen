ee110539fd0996e63ffd35f2c1c6636b46187337
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-20 20:57:00 +0000

    refactor: depthEnd -> depthEndIdx

--- client/src/core/data/walk-bfs/index.ts
+++ client/src/core/data/walk-bfs/index.ts
@@ -4,7 +4,7 @@ export const makeWalkBfs = (getChildren: any, maxDepth = Infinity) =>
 		start: any,
 		next: any = [],
 		currentDepth = 0,
-		depthEnd = 0,
+		depthEndIdx = 0,
 		parents: any = [],
 	): any {
 		yield { parent: parents.at(0) ?? null, node: start };
@@ -22,7 +22,7 @@ export const makeWalkBfs = (getChildren: any, maxDepth = Infinity) =>
 				yield* walkBfs(
 					Object.values(children).at(0),
 					Object.values(children).slice(1),
-					!depthEnd ? currentDepth + 1 : currentDepth,
+					!depthEndIdx ? currentDepth + 1 : currentDepth,
 					Object.values(children).slice(1).length,
 					[
 						...parents.slice(1),
@@ -36,7 +36,7 @@ export const makeWalkBfs = (getChildren: any, maxDepth = Infinity) =>
 					next.at(0),
 					next.slice(1),
 					currentDepth,
-					depthEnd - 1,
+					depthEndIdx - 1,
 					[
 						...parents.slice(1),
 						...Array(children.length).fill(start),
@@ -46,8 +46,8 @@ export const makeWalkBfs = (getChildren: any, maxDepth = Infinity) =>
 				yield* walkBfs(
 					next.at(0),
 					[...next.slice(1), ...Object.values(children)],
-					!depthEnd ? currentDepth + 1 : currentDepth,
-					depthEnd ? depthEnd - 1 : next.slice(1).length,
+					!depthEndIdx ? currentDepth + 1 : currentDepth,
+					depthEndIdx ? depthEndIdx - 1 : next.slice(1).length,
 					[
 						...parents.slice(1),
 						...Array(children.length).fill(start),
