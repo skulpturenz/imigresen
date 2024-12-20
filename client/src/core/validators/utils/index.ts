c548f0ec4447e13d38847aba49b5cb01172cfa66
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-20 11:34:28 +0000

    fix: nullish

--- client/src/core/validators/utils/index.ts
+++ client/src/core/validators/utils/index.ts
@@ -2,18 +2,18 @@ export const makeWalkBfs = (getChildren: any) =>
 	function* walkBfs(start: any, next: any = []): any {
 		yield start;
 
-		if (!Object.values(getChildren(start)).length && !next.length) {
+		if (!Object.values(getChildren(start) ?? []).length && !next.length) {
 			return;
 		}
 
 		if (!next.length) {
-			yield* walkBfs(Object.values(getChildren(start)).at(0), [
-				...Object.values(getChildren(start)).slice(1),
+			yield* walkBfs(Object.values(getChildren(start) ?? []).at(0), [
+				...Object.values(getChildren(start) ?? []).slice(1),
 			]);
 		} else {
 			yield* walkBfs(next.at(0), [
 				...next.slice(1),
-				...Object.values(getChildren(start)),
+				...Object.values(getChildren(start) ?? []),
 			]);
 		}
 	};
