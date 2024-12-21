a8a8d9cb6d6e30806114563ee5ac7fc68cbda592
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-21 22:58:41 +0000

    fix: allow filter to operate on parents

--- client/src/core/data/walk/filter.test.ts
+++ client/src/core/data/walk/filter.test.ts
@@ -3,7 +3,12 @@ import { partialRight } from "es-toolkit";
 import { describe, expect, it } from "vitest";
 import { makeFilter } from "./filter";
 import { makeParents } from "./parents";
-import type { Node } from "./types";
+import type {
+	Node,
+	NodeWithGrandparents,
+	Walk,
+	WalkWithGrandparents,
+} from "./types";
 
 describe("filter", () => {
 	interface Tree {
@@ -59,22 +64,50 @@ describe("filter", () => {
 
 	it("returns a `Walk`", () => {
 		const walk = makeWalkBfs((tree: Tree) => tree.children);
-		const walkWithFilter = partialRight(
-			makeFilter(walk),
-			({ node }: Node<Tree>) => node.hello === "world2",
-		);
-		const walkWithGrandparentsAndFilter = makeParents(walkWithFilter);
-		const walkWithGrandparents = makeParents(walk);
 
-		expect(Array.from(walkWithGrandparentsAndFilter(tree))).toHaveLength(1);
+		const walkWithGrandparents = makeParents(walk);
+		const walkWithFilterAndGrandparents = makeParents(
+			partialRight(
+				makeFilter(walk),
+				({ node }: Node<Tree>) => node.hello === "world2",
+			) as Walk<Tree>,
+		);
+		const walkWithGrandparentsAndFilter = makeFilter(
+			makeParents(walk) as WalkWithGrandparents<Tree>,
+		);
+
 		// note: if there was no filter then there would be two items in `parents`
+		expect(Array.from(walkWithFilterAndGrandparents(tree))).toHaveLength(1);
 		expect(
 			Array.from(walkWithGrandparents(tree)).find(
 				({ node }) => node.hello === "world2",
 			)?.parents.length,
 		).toBeGreaterThan(
-			Array.from(walkWithGrandparentsAndFilter(tree))?.at(0)?.parents
+			Array.from(walkWithFilterAndGrandparents(tree))?.at(0)?.parents
 				.length ?? 0,
+		);
+
+		expect(
+			Array.from(
+				walkWithGrandparentsAndFilter(
+					tree,
+					({ node }) => node.hello === "world2",
+				),
+			),
+		).toHaveLength(1);
+		expect(
+			Array.from(walkWithGrandparents(tree)).find(
+				({ node }) => node.hello === "world2",
+			)?.parents.length,
+		).toBe(
+			(
+				Array.from(
+					walkWithGrandparentsAndFilter(
+						tree,
+						({ node }) => node.hello === "world2",
+					),
+				)?.at(0) as NodeWithGrandparents<Tree>
+			)?.parents.length ?? 0,
 		);
 	});
 });
--- client/src/core/data/walk/filter.ts
+++ client/src/core/data/walk/filter.ts
@@ -1,7 +1,15 @@
-import type { Node, Walk } from "./types";
+import type {
+	Node,
+	NodeWithGrandparents,
+	Walk,
+	WalkWithGrandparents,
+} from "./types";
 
-export const makeFilter = <T>(walk: Walk<T>) =>
-	function* filter(tree: T, predicate: (node: Node<T>) => boolean) {
+export const makeFilter = <T>(walk: Walk<T> | WalkWithGrandparents<T>) =>
+	function* filter(
+		tree: T,
+		predicate: (node: Node<T> | NodeWithGrandparents<T>) => boolean,
+	) {
 		for (const result of walk(tree)) {
 			if (!predicate(result)) {
 				continue;
