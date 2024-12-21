a743a7fec1375b7fcb1d9911da8789300acc5894
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-21 05:41:53 +0000

    feat: add parents

--- client/src/core/data/walk-bfs/walk-bfs.test.ts
+++ client/src/core/data/walk-bfs/walk-bfs.test.ts
@@ -1,5 +1,6 @@
 import { makeWalkBfs } from "core/data/walk-bfs";
 import { describe, expect, it } from "vitest";
+import { makeParents } from "./parents";
 
 describe("walk-bfs", () => {
 	interface Tree {
@@ -88,6 +89,70 @@ describe("walk-bfs", () => {
 
 		expect(Object.values(nodeParents)).toEqual(
 			Array.from(walk(tree), ({ parent }) => parent?.hello ?? null),
+		);
+	});
+});
+
+describe("parents", () => {
+	interface Tree {
+		hello: string;
+		children?: Tree[];
+	}
+
+	const tree: Tree = {
+		hello: "world", // depth = 0
+		children: [
+			{
+				hello: "world1", // depth = 1
+				children: [
+					{
+						hello: "world2", // depth = 2
+					},
+				],
+			},
+			{
+				hello: "world3", // depth = 1
+				children: [
+					{
+						hello: "world4", // depth = 2
+						children: [
+							{
+								hello: "world5", // depth = 3
+								children: [
+									{
+										hello: "world6", // depth = 4
+										children: [
+											{
+												hello: "world7", // depth = 5
+											},
+										],
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+		],
+	};
+
+	it("has all parents for a node", () => {
+		const walk = makeWalkBfs((tree: Tree) => tree.children);
+		const parents = makeParents(walk);
+
+		const nodeParents = {
+			world: 0,
+			world1: 1,
+			world3: 1,
+			world2: 2,
+			world4: 2,
+			world5: 3,
+			world6: 4,
+			world7: 5,
+		};
+
+		expect(Object.values(nodeParents)).toEqual(
+			Array.from(parents(tree), ({ parents }) => parents.length),
 		);
 	});
 });
