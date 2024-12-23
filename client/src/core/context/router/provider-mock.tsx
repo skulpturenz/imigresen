128563727c51abf5a018f2d9df0d6b893d0bf009
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-23 08:02:58 +0000

    feat: setup mock providers

--- client/src/core/context/router/provider-mock.tsx
+++ client/src/core/context/router/provider-mock.tsx
@@ -1,17 +1,30 @@
-import { type Accessor, type Component, type ParentProps } from "solid-js";
+import {
+	mergeProps,
+	type Accessor,
+	type Component,
+	type ParentProps,
+} from "solid-js";
 import { RouterContext } from "./provider";
-import { type RouterSvc } from "./store";
+import { useStore, type RouterSvc } from "./store";
 
 export interface RouterProviderMockProps {
-	svc: Accessor<RouterSvc>;
+	svc?: Accessor<RouterSvc>;
 }
 
-export const RouteProviderMock: Component<
+export const RouterProviderMock: Component<
 	ParentProps<RouterProviderMockProps>
 > = props => {
+	const value = useStore();
+	const withDefaultProps = mergeProps(
+		{
+			svc: value,
+		},
+		props,
+	);
+
 	return (
-		<RouterContext.Provider value={props.svc}>
-			{props.children}
+		<RouterContext.Provider value={withDefaultProps.svc}>
+			{withDefaultProps.children}
 		</RouterContext.Provider>
 	);
 };
--- client/src/core/context/ui/provider-mock.tsx
+++ client/src/core/context/ui/provider-mock.tsx
@@ -1,22 +1,79 @@
 import {
+	ColorModeProvider,
+	ColorModeScript,
+	I18nProvider,
+	localStorageManager,
+} from "@kobalte/core";
+import {
+	createEffect,
+	mergeProps,
 	Show,
 	type Accessor,
 	type Component,
 	type ParentProps,
 } from "solid-js";
+import { ToastList, ToastRegion } from "ui/toast";
 import { UiContext } from "./provider";
-import { type UiSvc } from "./store";
+import { useStore, type UiSvc } from "./store";
 
 export interface UiProviderMockProps {
-	svc: Accessor<UiSvc>;
+	svc?: Accessor<UiSvc>;
 }
 
 export const UiProviderMock: Component<
 	ParentProps<UiProviderMockProps>
 > = props => {
+	const value = useStore();
+	const withDefaultProps = mergeProps(
+		{
+			svc: value,
+		},
+		props,
+	);
+
+	createEffect(() => {
+		if (value().isInitialLoading()) {
+			return;
+		}
+
+		if (!import.meta.env.SSR) {
+			const root = window.document.documentElement;
+
+			root.classList.remove("light", "dark");
+
+			if (value().theme === "system") {
+				const systemTheme = window.matchMedia(
+					"(prefers-color-scheme: dark)",
+				).matches
+					? "dark"
+					: "light";
+
+				root.classList.add(systemTheme);
+
+				return;
+			}
+
+			root.classList.add(value().theme);
+		}
+	});
+
 	return (
-		<UiContext.Provider value={props.svc}>
-			<Show when={!props.svc().isInitialLoading}>{props.children}</Show>
+		<UiContext.Provider value={withDefaultProps.svc}>
+			<Show when={!withDefaultProps.svc().isInitialLoading()}>
+				<I18nProvider locale={withDefaultProps.svc().locale}>
+					<ColorModeScript storageType="localStorage" />
+
+					<ColorModeProvider
+						initialColorMode={withDefaultProps.svc().theme}
+						storageManager={localStorageManager}>
+						{withDefaultProps.children}
+
+						<ToastRegion>
+							<ToastList />
+						</ToastRegion>
+					</ColorModeProvider>
+				</I18nProvider>
+			</Show>
 		</UiContext.Provider>
 	);
 };
