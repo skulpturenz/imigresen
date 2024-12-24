5093cec19c780595531ea9a730568aad5d5c5715
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-24 12:08:21 +0000

    chore: revert change to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import { ProvidersMock as Providers } from "core/context/provider-mock";
-// import { Providers } from "core/context";
+// import { ProvidersMock as Providers } from "core/context/provider-mock";
+import { Providers } from "core/context";
 import { ErrorBoundary, type Component, type ParentProps } from "solid-js";
 import { Portal } from "solid-js/web";
 import { Router } from "./core/router";
