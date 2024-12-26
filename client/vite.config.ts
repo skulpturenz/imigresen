12eaf91b45e2bb0145d59a76100b6ad9261d5613
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-12-26 04:42:20 +0000

    chore: add automerge

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,14 @@ import { join } from "node:path";
 import { defineConfig } from "vite";
 import { default as viteCompression } from "vite-plugin-compression";
 import { default as solid } from "vite-plugin-solid";
+import { default as topLevelAwait } from "vite-plugin-top-level-await";
+import { default as wasm } from "vite-plugin-wasm";
 import { default as webfontDownload } from "vite-plugin-webfont-dl";
 
 export default defineConfig({
 	plugins: [
+		wasm(),
+		topLevelAwait(),
 		solid(),
 		webfontDownload(),
 		viteCompression({
