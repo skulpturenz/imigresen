eed23e77adfc57568f071a19ffcffc3837b4513a
Tyrone Biggums <55116576+nmathew98@users.noreply.github.com>
2025-01-03 18:00:18 +1300

    feat: navbar items sorting (#70)

--- client/src/core/ui/shell/navbar.tsx
+++ client/src/core/ui/shell/navbar.tsx
@@ -37,6 +37,8 @@ import {
 	NavigationMenuTrigger,
 } from "ui/navigation-menu";
 import { cn } from "ui/utils";
+import type { NavbarItem } from "./types";
+import { sortNavbarItems, sortNavigationRoutes } from "./utils";
 
 const resources = {
 	logoAlt: "Imigresen",
@@ -198,67 +200,81 @@ export const Navbar: Component<ParentProps> = () => {
 		</div>
 	);
 
-	const getNavigationMenuItems = () => {
-		const menuItems = Object.values(routerContext().routes)
-			.filter(
-				route =>
-					!route.info?.isHidden &&
-					route.info?.isAllowed &&
-					isTopLevelRoute(route),
-			)
-			.map(route => ({
-				trigger: route,
-				children: getAllChildren(route),
-			}));
+	const getNavbarItems = () => {
+		const navbarItems = Object.values(routerContext().routes)
+			.reduce<NavbarItem[]>((acc, route) => {
+				if (
+					route.info?.isHidden ||
+					!route.info?.isAllowed ||
+					!isTopLevelRoute(route)
+				) {
+					return acc;
+				}
 
-		return menuItems;
+				return [
+					...acc,
+					{
+						trigger: route,
+						children:
+							getAllChildren(route).sort(sortNavigationRoutes),
+					},
+				];
+			}, [])
+			.sort(sortNavbarItems);
+
+		return navbarItems;
 	};
 
-	const navigationMenuItems = createMemo(getNavigationMenuItems);
+	const navbarItems = createMemo(getNavbarItems);
 
 	const Navbar = () => (
 		<NavigationMenu>
-			<For each={navigationMenuItems()}>
-				{menuItem => (
+			<For each={navbarItems()}>
+				{navbarItem => (
 					<>
-						<Show when={!menuItem.children.length}>
+						<Show when={!navbarItem.children.length}>
 							<NavigationMenuTrigger
 								as="a"
-								href={menuItem.trigger.info?.hrefPath}>
-								{menuItem.trigger.meta?.navigationConfig
-									?.title || menuItem.trigger.title}
+								href={navbarItem.trigger.info?.hrefPath}>
+								{navbarItem.trigger.meta?.navigationConfig
+									?.title || navbarItem.trigger.title}
 							</NavigationMenuTrigger>
 						</Show>
 
-						<Show when={menuItem.children.length}>
+						<Show when={navbarItem.children.length}>
 							<NavigationMenuItem>
 								<NavigationMenuTrigger
 									as="a"
-									href={menuItem.trigger.info?.hrefPath}>
-									{menuItem.trigger.meta?.navigationConfig
-										?.title || menuItem.trigger.title}
+									href={navbarItem.trigger.info?.hrefPath}>
+									{navbarItem.trigger.meta?.navigationConfig
+										?.title || navbarItem.trigger.title}
 								</NavigationMenuTrigger>
 
 								<NavigationMenuContent>
-									<For each={menuItem.children}>
-										{link => (
+									<For each={navbarItem.children}>
+										{navbarItemChild => (
 											<NavigationMenuLink
-												href={link.info?.hrefPath}>
+												href={
+													navbarItemChild.info
+														?.hrefPath
+												}>
 												<NavigationMenuItemLabel>
-													{link.meta?.navigationConfig
+													{navbarItemChild.meta
+														?.navigationConfig
 														?.title ||
-														menuItem.trigger.title}
+														navbarItem.trigger
+															.title}
 												</NavigationMenuItemLabel>
 
 												<Show
 													when={
-														link.meta
+														navbarItemChild.meta
 															?.navigationConfig
 															?.description
 													}>
 													<NavigationMenuDescription>
 														{
-															link.meta
+															navbarItemChild.meta
 																?.navigationConfig
 																?.description
 														}
@@ -311,7 +327,7 @@ export const Navbar: Component<ParentProps> = () => {
 							</div>
 						</div>
 
-						<Show when={navigationMenuItems().length}>
+						<Show when={navbarItems().length}>
 							<Navbar />
 						</Show>
 					</div>
