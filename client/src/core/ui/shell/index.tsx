851f6745a0ea7dc75f8f1cc21818bb6b2a4ab682
Tyrone Biggums <55116576+nmathew98@users.noreply.github.com>
2025-01-05 00:51:39 +1300

    chore: remove tsparticles (#73)

--- client/src/core/ui/shell/index.tsx
+++ client/src/core/ui/shell/index.tsx
@@ -1,21 +1,8 @@
 import { useLocale } from "@kobalte/core";
-/// @ts-expect-error: export error
-import { initParticlesEngine, default as Particles } from "@tsparticles/solid";
 import { styles } from "core/constants/styles";
-import { UiContext } from "core/context/ui";
-import { useContext } from "core/context/utils";
-import {
-	createEffect,
-	createSignal,
-	createUniqueId,
-	Show,
-	type Component,
-	type ParentProps,
-} from "solid-js";
-import { loadFull } from "tsparticles";
+import { type Component, type ParentProps } from "solid-js";
 import { cn } from "ui/utils";
 import { Navbar } from "./navbar";
-import { particlesConfig } from "./particles-config";
 
 export const Shell: Component<ParentProps> = props => {
 	const { locale, direction } = useLocale();
@@ -31,43 +18,6 @@ export const Shell: Component<ParentProps> = props => {
 					</div>
 				</div>
 			</div>
-
-			<ParticlesBackground />
 		</div>
-	);
-};
-
-const ParticlesBackground = () => {
-	const [init, setInit] = createSignal(false);
-	const uiContext = useContext(UiContext);
-
-	createEffect(previousTheme => {
-		const theme = uiContext().theme;
-
-		if (previousTheme === theme && init()) {
-			return;
-		}
-
-		setInit(false);
-
-		initParticlesEngine((engine: any) => {
-			// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-			// starting from v2 you can add only the features you need reducing the bundle size
-			return loadFull(engine);
-		}).then(() => {
-			setInit(true);
-		});
-
-		return theme;
-	});
-
-	return (
-		<Show when={init()}>
-			<Particles
-				className="absolute"
-				id={createUniqueId()}
-				options={particlesConfig}
-			/>
-		</Show>
 	);
 };
