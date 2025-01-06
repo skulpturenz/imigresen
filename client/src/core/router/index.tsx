a4b9301f4d24d2719b484a407667fa300bf97d19
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-06 04:58:38 +0000

    feat: update RouterErrorBoundary

--- client/src/core/router/index.tsx
+++ client/src/core/router/index.tsx
@@ -1,10 +1,22 @@
 import { Router as SolidRouter } from "@solidjs/router";
+import { useI18n } from "core/context/i18n";
 import { Router as AuthRouter } from "feat/auth";
 import { Router as HomeRouter } from "feat/home";
 import { Router as NotFoundRouter } from "feat/not-found";
 import { Router as UnauthorizedRouter } from "feat/unauthorized";
 import { ErrorBoundary, type Component, type ParentProps } from "solid-js";
-import { Portal } from "solid-js/web";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogClose,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "ui/alert-dialog";
+import { withI18n } from "./resources";
+import type { resources } from "./resources/i18n/en-US";
 
 export type RouterProps = Record<string, unknown>;
 
@@ -21,14 +33,45 @@ export const Router = () => {
 	);
 };
 
-// TODO: improve
 const RouterErrorBoundary: Component<ParentProps> = props => (
 	<ErrorBoundary
-		fallback={(err, reset) => (
-			<Portal>
-				<div onClick={reset}>Error: {err.toString()}</div>
-			</Portal>
-		)}>
+		fallback={(_err, reset) => {
+			const Fallback = withI18n(() => {
+				const t = useI18n<typeof resources>();
+
+				const onClickCancel = reset;
+				const onClickBackToHome = () => {
+					reset();
+
+					location.pathname = "/";
+				};
+
+				return (
+					<AlertDialog defaultOpen>
+						<AlertDialogContent>
+							<AlertDialogHeader>
+								<AlertDialogTitle>
+									{t("errorBoundary.title")}
+								</AlertDialogTitle>
+								<AlertDialogDescription>
+									{t("errorBoundary.description")}
+								</AlertDialogDescription>
+							</AlertDialogHeader>
+							<AlertDialogFooter>
+								<AlertDialogClose onClick={onClickCancel}>
+									{t("errorBoundary.doCancel")}
+								</AlertDialogClose>
+								<AlertDialogAction onClick={onClickBackToHome}>
+									{t("errorBoundary.doBackToHome")}
+								</AlertDialogAction>
+							</AlertDialogFooter>
+						</AlertDialogContent>
+					</AlertDialog>
+				);
+			});
+
+			return <Fallback />;
+		}}>
 		{props.children}
 	</ErrorBoundary>
 );
---
+++ client/src/core/router/resources/i18n/en-US.ts
@@ -0,0 +1,9 @@
+export const resources = {
+	errorBoundary: {
+		title: "An unexpected error occurred",
+		description:
+			"Go back to home or click cancel to continue on the current page.",
+		doCancel: "Cancel",
+		doBackToHome: "Back to home",
+	},
+};
