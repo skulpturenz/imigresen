103794efe0312d1429eba8cafe0653e6cffafd46
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-10 21:36:17 +0000

    fix: update schema

--- client/src/feat/new-passport/schema.ts
+++ client/src/feat/new-passport/schema.ts
@@ -1,31 +1,29 @@
-import { boolean, number, object, string } from "yup";
+import { boolean, date, number, object, string } from "yup";
 
 export const schema = object({
-	documentType: string().required(),
-	requestType: string(),
+	type: string(),
+	applicationType: string(),
+	fullName: string(),
+	otherName: string(),
+	identityCardNumber: string(),
+	birthCertificateNumber: string(),
+	dateOfBirth: date(),
+	gender: string(),
+	currentAddress: string(),
+	postcode: string(),
+	city: string(),
+	state: string(),
+	country: string(),
+	height: number(), // meters
+	relatonshipStatus: string(),
+	mobileNumber: string(),
 	currentPassportNumber: string(),
-	personalDetails: object({
-		fullName: string(),
-		otherName: string(),
-		identityCardNumber: string(),
-		birthDocumentNumber: string(),
-		stateOfBirth: string(),
-		gender: number(),
-		currentAddress: string(),
-		postcode: string(),
-		city: string(),
-		state: string(),
-		height: number(),
-		relationshipStatus: string(),
-	}),
-	declaration: object({
-		affirmValidity: boolean(),
-		affirmCurrentPassportStatus: boolean(),
-		affirmChildrenPictures: boolean(),
-		affirmTrueAndValid: boolean(),
-		signature: string(), // base64
-		affirmChildrenParentName: string(),
-		affirmChildrenParentIdentityCardNumber: string(),
-		affirmChildrenParentSignature: string(), // base64
-	}),
+	isPassportLost: boolean(),
+	isDependentRequest: boolean(),
+	isPersonalDetailsValid: boolean(),
+	isDependentPictureCurrent: boolean(),
+	isAllInformationValid: boolean(),
+	principalCaregiverFullName: string(),
+	principalCaregiverIdentityCardNumber: string(),
+	principalCaregiverSignature: string(),
 });
