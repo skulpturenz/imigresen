46a19b062707b4872398031a1d7a7e9708b4713a
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-12 05:43:58 +0000

    feat: add findOptionalFieldPaths

--- client/src/core/data/yup/utils.ts
+++ client/src/core/data/yup/utils.ts
@@ -1,6 +1,8 @@
+import { makeWalkBfs } from "core/data//walk";
+import { makeParents } from "core/data/walk/parents";
 import { isNil } from "es-toolkit";
 import { get } from "es-toolkit/compat";
-import type { Schema } from "yup";
+import type { Schema, SchemaDescription, SchemaObjectDescription } from "yup";
 
 // TODO: options types
 export const whenOptions =
@@ -38,3 +40,36 @@ export const hasSomeParentField =
 export const isParentFieldEqual =
 	(path: string, value: any) => (options: any) =>
 		get(options.parent, path) === value;
+
+export const findOptionalFieldPaths = <T extends Schema>(schema: T) => {
+	const description = schema.describe({
+		context: {
+			isFinal: true,
+		},
+	}) as SchemaObjectDescription;
+	const walk = makeWalkBfs(
+		(parent: SchemaDescription) =>
+			Object.values(
+				(parent as SchemaObjectDescription)?.fields ??
+					Object.create(null),
+			) as SchemaDescription[],
+	);
+	const walkWithParents = makeParents(walk);
+
+	const requiredFields = new Set<string>();
+
+	for (const { node, parents } of walkWithParents(description)) {
+		// TODO: update `walk` to yield from `Object.entries`
+		const path = parents.join(".");
+
+		if (node.tests.some(test => test.name === "required")) {
+			requiredFields.add(path);
+		}
+
+		if (node.optional === false) {
+			requiredFields.add(path);
+		}
+	}
+
+	return requiredFields;
+};
