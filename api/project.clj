d3f2991a5df0d72d33d92bc30953c186dd0ca9c8
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-16 04:17:38 +0000

    chore: update deps

--- api/project.clj
+++ api/project.clj
@@ -1,9 +1,17 @@
 (defproject imigresen-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :description "Imigresen API"
+  :url "https://skulpture.xyz"
+  :license {:name "MIT"
+            :url "https://opensource.org/license/mit"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.stuartsierra/component "1.1.0"]
+                 [com.github.steffan-westcott/clj-otel-api "0.2.7"]
+                 [ring/ring-core "1.13.0"]
+                 [compojure "1.7.1"]
+                 [keycloak-clojure/keycloak-clojure "1.31.2"]
+                 [com.github.seancorfield/honeysql "2.6.1243"]
+                 [com.github.igrishaev/pg2-core "0.1.29"]
+                 [io.flipt/flipt-java "1.1.2"]]
   :main ^:skip-aot imigresen-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
