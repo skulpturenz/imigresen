c8f0f26d171097add2d0f154637e8152e69c53df
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-18 08:28:44 +0000

    feat: pg state

--- api/src/state/pg.clj
+++ api/src/state/pg.clj
@@ -4,18 +4,28 @@
    [environ.core]
    [pg.core]
    [clojure.string]
-   [pg.migrations.core]))
+   [pg.migrations.core]
+   [pg.pool]))
 
 ;; TODO: parse connection string
-(def config {:host (environ.core/env :pg-host)
-             :port (environ.core/env :pg-port)
-             :user (environ.core/env :pg-user)
-             :password (environ.core/env :pg-password)
-             :database (environ.core/env :pg-database)
-             :use-ssl (environ.core/env :pg-use-ssl)
-             :migrations-table (clojure.string/join "-" [(environ.core/env :pg-migrations-table) (environ.core/env :java-env)])
-             :migrations-path (environ.core/env :pg-migrations-path)})
+(def pg2 (agent {:config {:host (environ.core/env :pg-host)
+                          :port (environ.core/env :pg-port)
+                          :user (environ.core/env :pg-user)
+                          :password (environ.core/env :pg-password)
+                          :database (environ.core/env :pg-database)
+                          :use-ssl (environ.core/env :pg-use-ssl)
+                          :migrations-table (clojure.string/join "-" [(environ.core/env :pg-migrations-table) (environ.core/env :java-env)])
+                          :migrations-path (environ.core/env :pg-migrations-path)}}))
+
+(defn start []
+  (pg.migrations.core/migrate-all (:config @pg2))
+  (send pg2 assoc :pool (pg.pool/pool (:config @pg2))))
+
+(defn stop []
+  (when (not (nil? (:pool @pg2)))
+    (pg.pool/close (:pool @pg2))
+    (send pg2 dissoc :pool)))
 
 (mount.core/defstate pg
-  :start ((pg.core/connect config) (pg.migrations.core/migrate-all config))
-  :stop (pg.core/close config))
+  :start (start)
+  :stop (stop))
