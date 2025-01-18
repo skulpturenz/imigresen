da51779adb6721372226e92b5ebaa0ccab84b4b9
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-18 19:52:42 +0000

    feat: create pg2 config from connection string

--- api/src/imigresen_api/core/state/pg.clj
+++ api/src/imigresen_api/core/state/pg.clj
@@ -15,47 +15,42 @@
    (java.net URI)))
 
 (defn create-config
-  [connection-string
-   ssl-client-key-absolute-path?
-   ssl-client-cert-absolute-path?
-   ssl-cert-ca-absolute-path?]
-  (when (and (not (nil? ssl-client-key-absolute-path?)) (not (nil? ssl-client-cert-absolute-path?)))
-    [pg.ssl/context ssl-client-key-absolute-path? ssl-client-cert-absolute-path?])
-  (when (not (nil? ssl-cert-ca-absolute-path?))
-    (pg.ssl/context ssl-cert-ca-absolute-path?))
-  (when (and (not (nil? ssl-client-key-absolute-path?)) (not (nil? ssl-client-cert-absolute-path?)) (not (nil? ssl-cert-ca-absolute-path?)))
-    (pg.ssl/context ssl-client-key-absolute-path? ssl-client-cert-absolute-path? ssl-cert-ca-absolute-path?))
-  (let [uri (URI. connection-string)
-        ssl-mode (clojure.string/lower-case (clojure.walk/keywordize-keys (ring.util.codec/form-decode (.getQuery uri))))]
-    {:host (.getHost uri)
-     :port (.getPort uri)
-     :user (first (clojure.string/split ":" (.getUserInfo uri)))
-     :password (second (clojure.string/split ":" (.getUserInfo uri)))
-     ;; https://www.postgresql.org/docs/8.4/libpq-connect.html#LIBPQ-CONNECT-SSLMODE
-     :use-ssl (clojure.core.match/match ssl-mode
-                "disable" false
-                "allow" false
-                "prefer" true
-                "require" true
-                "verify-ca" true
-                "verify-full" true)}))
+  ([connection-string]
+   (let [uri (URI. connection-string)
+         ssl-mode (clojure.string/lower-case (clojure.walk/keywordize-keys (ring.util.codec/form-decode (.getQuery uri))))]
+     {:host (.getHost uri)
+      :port (.getPort uri)
+      :user (first (clojure.string/split ":" (.getUserInfo uri)))
+      :password (second (clojure.string/split ":" (.getUserInfo uri)))
+      ;; https://www.postgresql.org/docs/8.4/libpq-connect.html#LIBPQ-CONNECT-SSLMODE
+      :use-ssl (clojure.core.match/match ssl-mode
+                 "disable" false
+                 "allow" false
+                 "prefer" true
+                 "require" true
+                 "verify-ca" true
+                 "verify-full" true)}))
+  ([connection-string ssl-cert-ca-absolute-path?]
+   (pg.ssl/context ssl-cert-ca-absolute-path?)
+   (create-config connection-string))
+  ([connection-string ssl-client-key-absolute-path? ssl-client-cert-absolute-path?]
+   (pg.ssl/context ssl-client-key-absolute-path? ssl-client-cert-absolute-path?)
+   (create-config connection-string))
+  ([connection-string ssl-client-key-absolute-path? ssl-client-cert-absolute-path? ssl-cert-ca-absolute-path?]
+   (pg.ssl/context ssl-client-key-absolute-path? ssl-client-cert-absolute-path? ssl-cert-ca-absolute-path?)
+   (create-config connection-string)))
 
-;; TODO: parse connection string
-(def pg2 (agent {:config {:host (environ.core/env :pg-host)
-                          :port (environ.core/env :pg-port)
-                          :user (environ.core/env :pg-user)
-                          :password (environ.core/env :pg-password)
-                          :database (environ.core/env :pg-database)
-                          :use-ssl (environ.core/env :pg-use-ssl)
-                          :migrations-table (clojure.string/join
-                                             "-"
-                                             [(environ.core/env :pg-migrations-table)
-                                              (imigresen-api.core.env/env :java-env #{"production" "development"} "development")])
-                          :migrations-path (environ.core/env :pg-migrations-path)
-                          :pool-min-size (environ.core/env :pg-pool-min-size)
-                          :pool-max-size (environ.core/env :pg-pool-max-size)
-                          :pool-expire-threshold-ms (environ.core/env :pg-pool-expire-threshold-ms)
-                          :pool-borrow-conn-timeout-ms (environ.core/env :pg-pool-borrow-conn-timeout-ms)}}))
+(def pg2 (agent {:config (merge
+                          (create-config (imigresen-api.core.env/env :pg-connection-string string?))
+                          {:migrations-table (clojure.string/join
+                                              "-"
+                                              [(environ.core/env :pg-migrations-table)
+                                               (imigresen-api.core.env/env :java-env #{"production" "development"} "development")])
+                           :migrations-path (environ.core/env :pg-migrations-path)
+                           :pool-min-size (environ.core/env :pg-pool-min-size)
+                           :pool-max-size (environ.core/env :pg-pool-max-size)
+                           :pool-expire-threshold-ms (environ.core/env :pg-pool-expire-threshold-ms)
+                           :pool-borrow-conn-timeout-ms (environ.core/env :pg-pool-borrow-conn-timeout-ms)})}))
 
 (defn start []
   (pg.migrations.core/migrate-all (:config @pg2))
