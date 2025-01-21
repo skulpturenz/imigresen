262bf61ca78e98f1dad307e8ed2f19256818c7bc
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-21 09:21:10 +0000

    chore: core formatting

--- api/src/imigresen_api/app/core.clj
+++ api/src/imigresen_api/app/core.clj
@@ -18,31 +18,25 @@
 
 (def app
   (ring-handler
-   (router
+   (router handlers {:exception reitit.dev.pretty/exception
+                     :data {:coercion reitit.coercion.spec/coercion
+                            :muuntaja muuntaja.core/instance
+                            :middleware [reitit.swagger/swagger-feature ;; swagger feature 
+                                         reitit.ring.middleware.parameters/parameters-middleware ;; query-params & form-params
+                                         reitit.ring.middleware.muuntaja/format-negotiate-middleware ;; content-negotiation
+                                         reitit.ring.middleware.muuntaja/format-response-middleware ;; encoding response body
+                                         reitit.ring.middleware.exception/exception-middleware ;; exception handling
+                                         reitit.ring.middleware.muuntaja/format-request-middleware ;; decoding request body
+                                         reitit.ring.coercion/coerce-response-middleware ;; coercing response bodys
+                                         reitit.ring.coercion/coerce-request-middleware ;; coercing request parameters
+                                         ;; multipart
+                                         reitit.ring.middleware.multipart/multipart-middleware]}})
 
-    handlers
-
-    {:exception reitit.dev.pretty/exception
-     :data {:coercion reitit.coercion.spec/coercion
-            :muuntaja muuntaja.core/instance
-            :middleware [reitit.swagger/swagger-feature ;; swagger feature 
-                         reitit.ring.middleware.parameters/parameters-middleware ;; query-params & form-params
-                         reitit.ring.middleware.muuntaja/format-negotiate-middleware ;; content-negotiation
-                         reitit.ring.middleware.muuntaja/format-response-middleware ;; encoding response body
-                         reitit.ring.middleware.exception/exception-middleware ;; exception handling
-                         reitit.ring.middleware.muuntaja/format-request-middleware ;; decoding request body
-                         reitit.ring.coercion/coerce-response-middleware ;; coercing response bodys
-                         reitit.ring.coercion/coerce-request-middleware ;; coercing request parameters
-                         ;; multipart
-                         reitit.ring.middleware.multipart/multipart-middleware]}})
-
-   (routes
-    ;; oauth
-    (create-swagger-ui-handler
-     {:path "/docs"
-      :config {:validatorUrl nil
-               :urls [{:name "swagger" :url "swagger.json"}]
-               :urls.primaryName "swagger"
-               :operationsSorter "alpha"}})
-
-    (create-default-handler [:not-found :method-not-allowed :not-acceptable]))))
+   (routes (create-swagger-ui-handler
+            ;; oauth
+            {:path "/docs"
+             :config {:validatorUrl nil
+                      :urls [{:name "swagger" :url "swagger.json"}]
+                      :urls.primaryName "swagger"
+                      :operationsSorter "alpha"}})
+           (create-default-handler [:not-found :method-not-allowed :not-acceptable]))))
