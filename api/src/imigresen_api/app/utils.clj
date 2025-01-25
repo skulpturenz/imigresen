1e3e1ea4bf7226d60bdb74be39e76cf0128c9658
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-25 06:28:05 +0000

    chore: remove comptime

--- api/src/imigresen_api/app/utils.clj
+++ api/src/imigresen_api/app/utils.clj
@@ -1,10 +1,5 @@
 (ns imigresen-api.app.utils)
 
-(defmacro comptime [form & args]
-  (if (ifn? form)
-    (apply (resolve (symbol form)) args)
-    form))
-
 (defmacro caught [form & args]
   (if (ifn? form)
     `(try (~form ~@args) (catch Exception e# e#))
--- api/src/imigresen_api/app/utils_test.clj
+++ api/src/imigresen_api/app/utils_test.clj
@@ -1,26 +1,11 @@
 (ns imigresen-api.app.utils-test
   (:require
-   [imigresen-api.app.utils :refer [caught comptime truthy]]
+   [imigresen-api.app.utils :refer [caught truthy]]
    [clojure.test :as t]
    [clojure.core.match :refer [match]]
    [clojure.string :as str])
   (:import
    (java.net URI)))
-
-(t/deftest comptime-fn
-  (t/testing "comptime fn n-args"
-    (t/is (= (comptime + 1 2 3 4 5) (+ 1 2 3 4 5)))))
-
-(t/deftest comptime-list
-  (t/testing "comptime list eval"
-    (t/is (= (comptime (+ 1 2 3 4 5)) (+ 1 2 3 4 5)))
-    (t/is (= (comptime '(1 2 3 4 5)) '(1 2 3 4 5)))))
-
-(t/deftest comptime-primitive
-  (t/testing "comptime primitive"
-    (t/is (= (comptime 1) 1))
-    ;; bad input
-    (t/is (= (comptime 1 2 3) 1))))
 
 (t/deftest caught-match
   (t/testing "caught"
