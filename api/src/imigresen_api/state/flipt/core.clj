82bfbea3c787182610df1d25ecdf5e79809c5d34
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-26 08:11:47 +0000

    feat: normalize responses to clojure map

--- api/src/imigresen_api/state/flipt/core.clj
+++ api/src/imigresen_api/state/flipt/core.clj
@@ -3,7 +3,12 @@
             [imigresen-api.app.env :refer [env]]
             [taoensso.telemere :as t])
   (:import (io.flipt.api FliptClient)
-           (io.flipt.api.evaluation.models EvaluationRequest BatchEvaluationRequest)
+           (io.flipt.api.evaluation.models EvaluationRequest
+                                           BatchEvaluationRequest
+                                           BooleanEvaluationResponse
+                                           BatchEvaluationResponse
+                                           VariantEvaluationResponse
+                                           EvaluationResponseType)
            (io.flipt.api.authentication ClientTokenAuthenticationStrategy)))
 
 (def flipt-agent (agent {}))
@@ -30,6 +35,40 @@
 (mount.core/defstate flipt-state
   :start (start))
 
+(defn boolean-evaluation? [res]
+  (= (.getType res) EvaluationResponseType/BOOLEAN_EVALUATION_RESPONSE_TYPE))
+
+(defn variant-evaluation? [res]
+  (= (.getType res) EvaluationResponseType/VARIANT_EVALUATION_RESPONSE_TYPE))
+
+(defn error-evaluation? [res]
+  (= (.getType res) EvaluationResponseType/ERROR_EVALUATION_RESPONSE_TYPE))
+
+(defn enabled? [res]
+  (cond
+    (instance? res BooleanEvaluationResponse) (.isEnabled res)
+    (instance? res VariantEvaluationResponse) (.isMatch res)
+    (instance? res BatchEvaluationResponse) (map #((cond
+                                                     (boolean-evaluation? %) (enabled? (.getBooleanResponse %))
+                                                     (variant-evaluation? %) (enabled? (.getVariantResponse %)))) (seq (.getResponses res)))))
+
+(defn- normalize [res]
+  (let [base {:type (cond
+                      (boolean-evaluation? res) :boolean
+                      (variant-evaluation? res) :variant
+                      (error-evaluation? res) :error
+                      :else :unknown)
+              :enabled (enabled? res)
+              :flag-key (.getFlagKey res)
+              :reason (.getReason res)
+              :timestamp (.getTimestamp res)
+              :duration (.getRequestDurationMillis res)}]
+    (if (boolean-evaluation? base)
+      base
+      (merge base {:segment-keys (seq (.getSegmentKeys res))
+                   :variant-attachment (.getVariantAttachment res)
+                   :variant-key (.getVariantKey res)}))))
+
 (defn evaluation-request
   ([namespace flag entity context] (evaluation-request namespace flag entity context nil))
   ([namespace flag entity context reference] (-> EvaluationRequest/builder
@@ -44,12 +83,14 @@
 (defn evaluate-variant [client req]
   (-> client
       (.evaluation)
-      (.evaluateVariant req)))
+      (.evaluateVariant req)
+      (normalize)))
 
 (defn evaluate-boolean [client req]
   (-> client
       (.evaluation)
-      (.evaluateBoolean req)))
+      (.evaluateBoolean req)
+      (normalize)))
 
 (defn evaluate-batch [client batch-reqs]
   (let [batch (-> BatchEvaluationRequest/builder
@@ -57,4 +98,10 @@
                   (.build))]
     (-> client
         (.evaluation)
-        (.evaluateBatch batch))))
+        (.evaluateBatch batch)
+        (.getResponses batch)
+        (seq)
+        ((partial map #((cond
+                          (boolean-evaluation? %) (.getBooleanResponse %)
+                          (variant-evaluation? %) (.getVariantResponse %)))))
+        ((partial filter #((not (nil? %))))))))
