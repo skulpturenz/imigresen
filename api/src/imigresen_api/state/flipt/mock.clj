aa11160fe6fa4d22f0313e329353ba3b678c7e86
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-28 08:55:14 +0000

    docs: add todos

--- api/src/imigresen_api/state/flipt/mock.clj
+++ api/src/imigresen_api/state/flipt/mock.clj
@@ -1,7 +1,8 @@
 (ns imigresen-api.state.flipt.mock
   (:require [mount.core :refer [defstate]]
             [imigresen-api.state.flipt.core :refer [start stop]]
-            [org.httpkit.fake :refer [with-fake-http]]))
+            [org.httpkit.fake :refer [with-fake-http]]
+            [imigresen-api.app.routes :refer [status-codes]]))
 
 (def ^:private mock-flipt-url "https://localhost")
 
@@ -9,9 +10,14 @@
   :start (start mock-flipt-url "mock")
   :stop (stop))
 
-(defn boolean-evaluation [enabled] {"enabled" enabled})
+;; TODO: fn form might be better
+;; need to return a string or a partial of actual response map
+(defn boolean-evaluation [enabled] {"status" (:ok status-codes)
+                                    "enabled" enabled})
 
-(defn variant-evaluation [match variant attachment] {"match" match
+;; TODO: request id from original request
+(defn variant-evaluation [match variant attachment] {"status" (:ok status-codes)
+                                                     "match" match
                                                      "variant-key" variant
                                                      "variant-attachment" attachment})
 (defmacro with-mock [res & expr]
