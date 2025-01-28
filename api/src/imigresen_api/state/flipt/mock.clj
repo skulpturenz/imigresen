8be765d5270556ab29e397524bfb75b7c184c9e1
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-28 08:41:06 +0000

    feat: add with-mock macro

--- api/src/imigresen_api/state/flipt/mock.clj
+++ api/src/imigresen_api/state/flipt/mock.clj
@@ -1,9 +1,12 @@
 (ns imigresen-api.state.flipt.mock
   (:require [mount.core :refer [defstate]]
-            [imigresen-api.state.flipt.core :refer [start stop enabled? variant]]))
+            [imigresen-api.state.flipt.core :refer [start stop]]
+            [org.httpkit.fake :refer [with-fake-http]]))
+
+(def ^:private mock-flipt-url "https://localhost")
 
 (defstate flipt
-  :start (start "https://localhost" "mock")
+  :start (start mock-flipt-url "mock")
   :stop (stop))
 
 (defn boolean-evaluation [enabled] {"enabled" enabled})
@@ -11,3 +14,6 @@
 (defn variant-evaluation [match variant attachment] {"match" match
                                                      "variant-key" variant
                                                      "variant-attachment" attachment})
+(defmacro with-mock [res & expr]
+  `(with-fake-http [mock-flipt-url ~res]
+     ~expr))
