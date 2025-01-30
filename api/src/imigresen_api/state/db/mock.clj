f899d6c1b03279de71c60b571fe9e7bc776ff21e
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-01-30 06:53:44 +0000

    feat: add use-fixture macro for db

--- api/src/imigresen_api/state/db/mock.clj
+++ api/src/imigresen_api/state/db/mock.clj
@@ -6,3 +6,15 @@
 (defstate db
   :start (start "jdbc:duckdb:")
   :stop (stop))
+
+(defmacro use-fixture []
+  `(do
+     (require '[mount.core :as mount]
+              '[imigresen-api.state.db.mock]
+              '[imigresen-api.state.db.core])
+     (let [fixture# (fn []
+                      (mount/start #'imigresen-api.state.db.mock/db)
+                      (mount/start-with {#'imigresen-api.state.db.core/db imigresen-api.state.db.mock/db})
+                      (f#)
+                      (mount/stop))]
+       (t/use-fixtures :once fixture#))))
