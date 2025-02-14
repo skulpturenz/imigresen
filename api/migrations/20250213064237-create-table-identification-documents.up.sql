520feb1a113c60d1964684c7cf43a8a4c78c8441
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-02-14 08:58:36 +0000

    chore: fix typo

--- api/migrations/20250213064237-create-table-identification-documents.up.sql
+++ api/migrations/20250213064237-create-table-identification-documents.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS identification_documents (
     uuid UUID NOT NULL UNIQUE,
     country TEXT NOT NULL UNIQUE REFERENCES countries(code),
     identity_card_number TEXT,
-    birth_certificate_number TEXT, -- birth ceritficate / adoption number / overseas birth cert (borang w)
+    birth_certificate_number TEXT, -- birth certificate / adoption number / overseas birth cert (borang w)
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
     CONSTRAINT pk_identification_documents PRIMARY_KEY(user, uuid, country)
