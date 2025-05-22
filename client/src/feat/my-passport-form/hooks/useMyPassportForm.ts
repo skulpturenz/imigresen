add602008649c067a3d13b70b0183dc604bac49e
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-05-22 08:12:35 +0000

    fix: create new doc handle if its not existing

--- client/src/feat/my-passport-form/hooks/useMyPassportForm.ts
+++ client/src/feat/my-passport-form/hooks/useMyPassportForm.ts
@@ -1,7 +1,8 @@
 import { AuthnContext } from "core/context/authn";
 import { myPassportFormAutomergeRepoMock } from "feat/my-passport-form/services/my-passport-form-service-mock";
+import type { MyPassportForm } from "feat/my-passport-form/types";
 import { useDocHandle } from "solid-automerge";
-import { useContext } from "solid-js";
+import { createResource, useContext } from "solid-js";
 
 export const useMyPassportForm = () => {
 	const authnContext = useContext(AuthnContext);
@@ -10,11 +11,27 @@ export const useMyPassportForm = () => {
 		authnContext().keycloak?.token,
 	);
 
-	const handle = useDocHandle(undefined, {
-		repo: automergeRepo,
-	});
+	const getDocHandle = () => {
+		// TODO
+		const isExistingForm = false;
+
+		if (isExistingForm) {
+			return useDocHandle(undefined, {
+				repo: automergeRepo,
+			});
+		}
+
+		// TODO: ideally don't do this
+		const [handle] = createResource("", () =>
+			automergeRepo.create<MyPassportForm>({
+				hello: "world",
+			}),
+		);
+
+		return handle;
+	};
 
 	return {
-		handle,
+		handle: getDocHandle(),
 	};
 };
