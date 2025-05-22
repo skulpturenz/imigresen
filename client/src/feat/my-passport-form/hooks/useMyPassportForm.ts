4b8eb1b8e18ce70956b7f2466081a2132e803f4b
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-05-22 08:18:21 +0000

    fix: check for isExistingForm

--- client/src/feat/my-passport-form/hooks/useMyPassportForm.ts
+++ client/src/feat/my-passport-form/hooks/useMyPassportForm.ts
@@ -1,3 +1,4 @@
+import { useParams, useSearchParams } from "@solidjs/router";
 import { AuthnContext } from "core/context/authn";
 import { myPassportFormAutomergeRepoMock } from "feat/my-passport-form/services/my-passport-form-service-mock";
 import type { MyPassportForm } from "feat/my-passport-form/types";
@@ -5,17 +6,17 @@ import { useDocHandle } from "solid-automerge";
 import { createResource, useContext } from "solid-js";
 
 export const useMyPassportForm = () => {
+	// TODO
 	const authnContext = useContext(AuthnContext);
-
 	const automergeRepo = myPassportFormAutomergeRepoMock(
 		authnContext().keycloak?.token,
 	);
 
-	const getDocHandle = () => {
-		// TODO
-		const isExistingForm = false;
+	const routeParams = useParams<{ uuid?: string }>();
+	const [searchParams] = useSearchParams<{ automergeUrl?: string }>();
 
-		if (isExistingForm) {
+	const getDocHandle = () => {
+		if (routeParams.uuid && searchParams.automergeUrl) {
 			return useDocHandle(undefined, {
 				repo: automergeRepo,
 			});
