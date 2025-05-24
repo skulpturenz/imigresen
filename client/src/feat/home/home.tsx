27ab134d198642907c8ee3d61b92a2c3b6891942
Tyrone Biggums <55116576+nmathew98@users.noreply.github.com>
2025-05-25 00:01:31 +1200

    feat: basic rework of the home page (#126)

--- client/src/feat/home/home.tsx
+++ client/src/feat/home/home.tsx
@@ -4,6 +4,16 @@ import { useI18n } from "core/context/i18n";
 import { toPath } from "core/router/route";
 import { For, Show } from "solid-js";
 import { Button } from "ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "ui/card";
+import { Tooltip, TooltipContent, TooltipTrigger } from "ui/tooltip";
+import { Typography } from "ui/typography";
 import { usePassportApplications } from "./hooks/usePassportApplications";
 import type { resources } from "./resources/i18n/en-US";
 
@@ -20,8 +30,14 @@ export const Home = () => {
 			</div>
 
 			<Show when={!passportApplications.isLoading}>
-				<div class="bg-background">
-					<ul>
+				<Show when={!passportApplications.data?.length}>
+					<Typography variant="h3" class="text-center">
+						No applications yet!
+					</Typography>
+				</Show>
+
+				<Show when={passportApplications.data?.length}>
+					<div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
 						<For each={passportApplications.data}>
 							{item => {
 								const getHref = () => {
@@ -41,20 +57,102 @@ export const Home = () => {
 									return url.href;
 								};
 
+								const details = [
+									{
+										label: "Email",
+										description: "test@test.com",
+									},
+									{
+										label: "Mobile number",
+										description: "0234567890",
+									},
+									{
+										label: "Document type",
+										description:
+											"Malaysian passport (64 pages)",
+									},
+									{
+										label: "Current document number",
+										description: "A1234124",
+									},
+									{
+										label: "Status",
+										description: "In progress",
+									},
+								];
+
 								return (
-									<li class="text-foreground">
-										<A href={getHref()}>
-											{item.uuid} (First name:{" "}
-											{item.personalDetails.firstName},
-											Last name:{" "}
-											{item.personalDetails.lastName})
-										</A>
-									</li>
+									<A href={getHref()} class="group max-w-md">
+										<Card class="h-full">
+											<CardHeader>
+												<Tooltip>
+													<TooltipTrigger
+														as={CardTitle}
+														class="truncate">
+														{
+															item.personalDetails
+																.firstName
+														}
+														&nbsp;
+														{
+															item.personalDetails
+																.lastName
+														}
+													</TooltipTrigger>
+													<TooltipContent>
+														{
+															item.personalDetails
+																.firstName
+														}
+														&nbsp;
+														{
+															item.personalDetails
+																.lastName
+														}
+													</TooltipContent>
+												</Tooltip>
+												<CardDescription>
+													Malaysian passport
+												</CardDescription>
+											</CardHeader>
+											<CardContent>
+												<For each={details}>
+													{item => (
+														<div class="mb-4 grid grid-cols-[20px_1fr] items-start pb-4 last:mb-0 last:pb-0">
+															<span class="flex size-2 translate-y-1 bg-teal-500 dark:bg-teal-400" />
+
+															<div class="space-y-2">
+																<Typography
+																	variant="small"
+																	as="p">
+																	{item.label}
+																</Typography>
+
+																<Typography
+																	variant="small"
+																	as="p">
+																	{
+																		item.description
+																	}
+																</Typography>
+															</div>
+														</div>
+													)}
+												</For>
+											</CardContent>
+
+											<CardFooter>
+												<Button class="w-full group-hover:bg-primary/80">
+													Edit
+												</Button>
+											</CardFooter>
+										</Card>
+									</A>
 								);
 							}}
 						</For>
-					</ul>
-				</div>
+					</div>
+				</Show>
 			</Show>
 		</div>
 	);
