3081599c9a215b2a34def74544539b11545218c4
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-05-24 11:53:49 +0000

    feat: basic rework of the home page

--- client/src/feat/home/home.tsx
+++ client/src/feat/home/home.tsx
@@ -4,6 +4,16 @@ import { useI18n } from "core/context/i18n";
 import { toPath } from "core/router/route";
 import { For, Show } from "solid-js";
 import { Button } from "ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "ui/card";
+import { Tooltip, TooltipContent, TooltipTrigger } from "ui/tooltip";
+import { Typography } from "ui/typography";
 import { usePassportApplications } from "./hooks/usePassportApplications";
 import type { resources } from "./resources/i18n/en-US";
 
@@ -20,40 +30,120 @@ export const Home = () => {
 			</div>
 
 			<Show when={!passportApplications.isLoading}>
-				<div class="bg-background">
-					<ul>
-						<For each={passportApplications.data}>
-							{item => {
-								const getHref = () => {
-									const url = new URL(location.origin);
-									url.hash = location.hash;
+				<div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
+					<For each={passportApplications.data}>
+						{item => {
+							const getHref = () => {
+								const url = new URL(location.origin);
+								url.hash = location.hash;
 
-									const searchParams = new URLSearchParams({
-										automergeUrl: item.automergeUrl,
-									});
+								const searchParams = new URLSearchParams({
+									automergeUrl: item.automergeUrl,
+								});
 
-									url.pathname = MyPassportForm.Edit.replace(
-										":uuid",
-										item.uuid,
-									);
-									url.search = searchParams.toString();
-
-									return url.href;
-								};
-
-								return (
-									<li class="text-foreground">
-										<A href={getHref()}>
-											{item.uuid} (First name:{" "}
-											{item.personalDetails.firstName},
-											Last name:{" "}
-											{item.personalDetails.lastName})
-										</A>
-									</li>
+								url.pathname = MyPassportForm.Edit.replace(
+									":uuid",
+									item.uuid,
 								);
-							}}
-						</For>
-					</ul>
+								url.search = searchParams.toString();
+
+								return url.href;
+							};
+
+							const details = [
+								{
+									label: "Email",
+									description: "test@test.com",
+								},
+								{
+									label: "Mobile number",
+									description: "0234567890",
+								},
+								{
+									label: "Document type",
+									description:
+										"Malaysian passport (64 pages)",
+								},
+								{
+									label: "Current document number",
+									description: "A1234124",
+								},
+								{
+									label: "Status",
+									description: "In progress",
+								},
+							];
+
+							return (
+								<A href={getHref()} class="group max-w-md">
+									<Card class="h-full">
+										<CardHeader>
+											<Tooltip>
+												<TooltipTrigger
+													as={CardTitle}
+													class="truncate">
+													{
+														item.personalDetails
+															.firstName
+													}
+													&nbsp;
+													{
+														item.personalDetails
+															.lastName
+													}
+												</TooltipTrigger>
+												<TooltipContent>
+													{
+														item.personalDetails
+															.firstName
+													}
+													&nbsp;
+													{
+														item.personalDetails
+															.lastName
+													}
+												</TooltipContent>
+											</Tooltip>
+											<CardDescription>
+												Malaysian passport
+											</CardDescription>
+										</CardHeader>
+										<CardContent>
+											<For each={details}>
+												{item => (
+													<div class="mb-4 grid grid-cols-[20px_1fr] items-start pb-4 last:mb-0 last:pb-0">
+														<span class="flex size-2 translate-y-1 bg-teal-500 dark:bg-teal-400" />
+
+														<div class="space-y-2">
+															<Typography
+																variant="small"
+																as="p">
+																{item.label}
+															</Typography>
+
+															<Typography
+																variant="small"
+																as="p">
+																{
+																	item.description
+																}
+															</Typography>
+														</div>
+													</div>
+												)}
+											</For>
+										</CardContent>
+
+										<CardFooter>
+											<Button class="w-full group-hover:bg-primary/80">
+												Edit
+											</Button>
+										</CardFooter>
+									</Card>
+								</A>
+							);
+						}}
+					</For>
 				</div>
 			</Show>
 		</div>
