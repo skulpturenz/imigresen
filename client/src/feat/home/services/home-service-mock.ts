db51cbbd2faf981bb00789ed61457117fa125dce
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-05-24 00:34:25 +0000

    chore: fix build

--- client/src/feat/home/services/home-service-mock.ts
+++ client/src/feat/home/services/home-service-mock.ts
@@ -1,14 +1,7 @@
-import { randAlpha, toCollection } from "@ngneat/falso";
 import type { PassportApplication } from "../types";
 
 export const homeService = (_token?: string) => {
-	const getPassportApplications = () =>
-		toCollection(
-			() => ({
-				hello: randAlpha(),
-			}),
-			{ length: 100 },
-		) as PassportApplication[];
+	const getPassportApplications = () => [] as PassportApplication[];
 
 	return {
 		getPassportApplications,
