f3e5641411d2c9f57a4bdc3e9ceb0a692c0575b2
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-05-29 07:59:17 +0000

    chore: update dev env

--- client/src/core/context/ui/automerge.ts
+++ client/src/core/context/ui/automerge.ts
@@ -1,16 +1,17 @@
 import { Repo, type NetworkAdapterInterface } from "@automerge/automerge-repo";
 import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket";
 import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb";
+import { invariant } from "es-toolkit";
 
 const storage = new IndexedDBStorageAdapter(
 	`imigresen-automerge-${import.meta.env.MODE}`,
 	`imigresen-documents-${import.meta.env.MODE}`,
 );
 
+invariant(import.meta.env.VITE_CF_AUTOMERGE_WSS, "Automerge API not specified");
+
 const network: NetworkAdapterInterface[] = [
-	// TODO
-	new BrowserWebSocketClientAdapter("ws://localhost:8787/api/v1"),
-	// new BrowserWebSocketClientAdapter("ws://localhost:5173"),
+	new BrowserWebSocketClientAdapter(import.meta.env.VITE_CF_AUTOMERGE_WSS),
 ];
 
 export const repo = new Repo({
