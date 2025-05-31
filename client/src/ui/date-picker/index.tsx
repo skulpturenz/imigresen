7ce6c5d29ed5d1b47b7c918a25890f98d8252688
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-05-31 01:39:38 +0000

    fix: datepicker h-10

--- client/src/ui/date-picker/index.tsx
+++ client/src/ui/date-picker/index.tsx
@@ -213,7 +213,7 @@ export const DatePickerInput = (props: DatePickerInputProps) => (
 	<DatePickerPrimitive.Input
 		{...spreadProps(props)}
 		class={cn(
-			"w-full h-9 border border-border focus-visible:border-border bg-background px-3 py-1 text-sm text-foreground shadow-sm",
+			"w-full h-10 border border-border focus-visible:border-border bg-background px-3 py-1 text-sm text-foreground shadow-sm",
 			"placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
 			"focus-visible:ring-offset-background focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 transition-shadow",
 			props.class,
@@ -226,7 +226,7 @@ export const DatePickerTrigger = (props: DatePickerTriggerProps) => (
 		{...spreadProps(props)}
 		class={cn(
 			"transition-[box-shadow,background-color] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
-			"focus-visible:ring-ring flex items-center justify-center min-w-9 min-h-9 rounded-e-md border group",
+			"focus-visible:ring-ring flex items-center justify-center min-w-9 min-h-10 rounded-e-md border group",
 			"border-border bg-background text-foreground [&>svg]:size-4 hover:bg-accent disabled:cursor-not-allowed disabled:opacity-50",
 			props.class,
 		)}>
