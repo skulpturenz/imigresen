96d5626a9d77925ad0565ed081e70933a1809023
Tyrone Biggums <55116576+nmathew98@users.noreply.github.com>
2025-06-04 21:57:33 +1200

    fix: init mapbox only when init is called (#188)

--- client/src/core/context/authn/store/index.ts
+++ client/src/core/context/authn/store/index.ts
@@ -35,191 +35,203 @@ export interface AuthnSvc {
 	};
 }
 
-export const useStore = createWithSignal<AuthnSvc>((set, get) => {
-	const authnProviderUrl = import.meta.env.VITE_KEYCLOAK_URL;
-	const authnProviderRealm = import.meta.env.VITE_KEYCLOAK_REALM;
-	const authnProviderClientId = import.meta.env.VITE_KEYCLOAK_CLIENT_ID;
+export interface AuthSvcInternal {
+	refreshMapboxTokenInterval?: ReturnType<typeof setInterval>;
+}
 
-	const stripPath = (url: URL | string) => {
-		const withoutPath = new URL((url as URL).href || url);
+export const useStore = createWithSignal<AuthnSvc & AuthSvcInternal>(
+	(set, get) => {
+		const authnProviderUrl = import.meta.env.VITE_KEYCLOAK_URL;
+		const authnProviderRealm = import.meta.env.VITE_KEYCLOAK_REALM;
+		const authnProviderClientId = import.meta.env.VITE_KEYCLOAK_CLIENT_ID;
 
-		withoutPath.pathname = "";
-		withoutPath.search = "";
-		withoutPath.hash = "";
+		const stripPath = (url: URL | string) => {
+			const withoutPath = new URL((url as URL).href || url);
 
-		return withoutPath;
-	};
-	const authnProviderClientUrl = stripPath(window.location.href);
+			withoutPath.pathname = "";
+			withoutPath.search = "";
+			withoutPath.hash = "";
 
-	invariant(authnProviderUrl, "Keycloak URL not specified");
-	invariant(authnProviderRealm, "Keycloak realm not specified");
-	invariant(authnProviderClientId, "Keycloak client ID not specified");
-	invariant(authnProviderClientUrl, "Keycloak client URL not specified");
+			return withoutPath;
+		};
+		const authnProviderClientUrl = stripPath(window.location.href);
 
-	const loginRedirectUri = [
-		trimEnd(authnProviderClientUrl.href, "/"),
-		toPath(CoreRoute.Auth, AuthRoute.LoginCallback),
-	].join("");
-	const logoutRedirectUri = [
-		trimEnd(authnProviderClientUrl.href, "/"),
-		toPath(CoreRoute.Auth, AuthRoute.LogoutCallback),
-	].join("");
+		invariant(authnProviderUrl, "Keycloak URL not specified");
+		invariant(authnProviderRealm, "Keycloak realm not specified");
+		invariant(authnProviderClientId, "Keycloak client ID not specified");
+		invariant(authnProviderClientUrl, "Keycloak client URL not specified");
 
-	const setAuthCookie = () => {
-		if (!get().keycloak?.token) {
-			return;
-		}
+		const loginRedirectUri = [
+			trimEnd(authnProviderClientUrl.href, "/"),
+			toPath(CoreRoute.Auth, AuthRoute.LoginCallback),
+		].join("");
+		const logoutRedirectUri = [
+			trimEnd(authnProviderClientUrl.href, "/"),
+			toPath(CoreRoute.Auth, AuthRoute.LogoutCallback),
+		].join("");
 
-		const cookie = Cookies.set(
-			storageKeys.authCookie,
-			get().keycloak?.token ?? "",
-			{
-				domain: `.${window.location.hostname}`,
-				expires: new Date(
-					secondsToMilliseconds(
-						get().keycloak?.tokenParsed?.exp ?? 0,
+		const setAuthCookie = () => {
+			if (!get().keycloak?.token) {
+				return;
+			}
+
+			const cookie = Cookies.set(
+				storageKeys.authCookie,
+				get().keycloak?.token ?? "",
+				{
+					domain: `.${window.location.hostname}`,
+					expires: new Date(
+						secondsToMilliseconds(
+							get().keycloak?.tokenParsed?.exp ?? 0,
+						),
 					),
-				),
+					secure: import.meta.env.PROD,
+					sameSite: "Strict",
+				},
+			);
+
+			if (!cookie) {
+				return;
+			}
+
+			document.cookie = cookie;
+		};
+
+		const deleteAuthCookie = () => {
+			// https://github.com/js-cookie/js-cookie?tab=readme-ov-file#basic-usage
+			// need to use same attributes for `path`, `domain`, `secure` and `sameSite`
+			Cookies.remove(storageKeys.authCookie, {
+				domain: `.${window.location.hostname}`,
 				secure: import.meta.env.PROD,
 				sameSite: "Strict",
-			},
-		);
+			});
+		};
 
-		if (!cookie) {
-			return;
-		}
+		const initMapbox = () => {
+			const mapboxClient = mbxClient({
+				accessToken: import.meta.env.VITE_MAPBOX_ACCESS_TOKEN,
+			});
+			const mapboxTokensClient = tokensClient(mapboxClient);
 
-		document.cookie = cookie;
-	};
+			const REFRESH_INTERVAL_SECONDS = 5;
+			const getTempMapboxToken = async () => {
+				const { body } = await mapboxTokensClient
+					.createTemporaryToken({
+						scopes: ["datasets:read"],
+						expires: addSeconds(
+							new Date(),
+							REFRESH_INTERVAL_SECONDS * 1.5,
+						).toISOString(),
+					})
+					.send();
 
-	const deleteAuthCookie = () => {
-		// https://github.com/js-cookie/js-cookie?tab=readme-ov-file#basic-usage
-		// need to use same attributes for `path`, `domain`, `secure` and `sameSite`
-		Cookies.remove(storageKeys.authCookie, {
-			domain: `.${window.location.hostname}`,
-			secure: import.meta.env.PROD,
-			sameSite: "Strict",
-		});
-	};
+				set({ mapboxToken: body.token });
+			};
 
-	const mapboxClient = mbxClient({
-		accessToken: import.meta.env.VITE_MAPBOX_ACCESS_TOKEN,
-	});
-	const mapboxTokensClient = tokensClient(mapboxClient);
+			const refreshMapboxTokenInterval = setInterval(
+				getTempMapboxToken,
+				secondsToMilliseconds(REFRESH_INTERVAL_SECONDS),
+			);
 
-	const REFRESH_INTERVAL_SECONDS = 5;
-	const getTempMapboxToken = async () => {
-		const { body } = await mapboxTokensClient
-			.createTemporaryToken({
-				scopes: ["datasets:read"],
-				expires: addSeconds(
-					new Date(),
-					REFRESH_INTERVAL_SECONDS * 1.5,
-				).toISOString(),
-			})
-			.send();
+			getTempMapboxToken();
 
-		set({ mapboxToken: body.token });
-	};
+			return refreshMapboxTokenInterval;
+		};
 
-	const refreshMapboxTokenInterval = setInterval(
-		getTempMapboxToken,
-		secondsToMilliseconds(REFRESH_INTERVAL_SECONDS),
-	);
+		return {
+			isInitialLoading: true,
+			isActionsLoading: false,
+			profile: null,
+			keycloak: new Keycloak({
+				url: authnProviderUrl,
+				realm: authnProviderRealm,
+				clientId: authnProviderClientId,
+			}),
+			userId: crypto.randomUUID(),
+			actions: {
+				init: once(async () => {
+					invariant(get().keycloak, "Keycloak instance not defined");
 
-	return {
-		isInitialLoading: true,
-		isActionsLoading: false,
-		profile: null,
-		keycloak: new Keycloak({
-			url: authnProviderUrl,
-			realm: authnProviderRealm,
-			clientId: authnProviderClientId,
-		}),
-		userId: crypto.randomUUID(),
-		actions: {
-			init: once(async () => {
-				invariant(get().keycloak, "Keycloak instance not defined");
+					set({ isInitialLoading: true });
 
-				set({ isInitialLoading: true });
+					await get().keycloak?.init({
+						onLoad: "check-sso",
+						silentCheckSsoRedirectUri: `${location.origin}/silent-check-sso.html`,
+						scope: "openid roles profile email address",
+						redirectUri: createRedirectUrl(
+							loginRedirectUri,
+							location.pathname,
+						).href,
+					});
 
-				await getTempMapboxToken();
+					set({ refreshMapboxTokenInterval: initMapbox() });
 
-				await get().keycloak?.init({
-					onLoad: "check-sso",
-					silentCheckSsoRedirectUri: `${location.origin}/silent-check-sso.html`,
-					scope: "openid roles profile email address",
-					redirectUri: createRedirectUrl(
-						loginRedirectUri,
-						location.pathname,
-					).href,
-				});
+					if (!get().keycloak?.authenticated) {
+						set({ isInitialLoading: false });
 
-				if (!get().keycloak?.authenticated) {
+						return;
+					}
+
+					const keycloak = get().keycloak;
+					invariant(keycloak, "Keycloak is initialized incorrectly");
+
+					const profile = await get().keycloak?.loadUserProfile();
+
+					if (!import.meta.env.SSR) {
+						setAuthCookie();
+						keycloak.onAuthRefreshSuccess = setAuthCookie;
+
+						window.localStorage.setItem(
+							AUTHN_SVC_SUB_CONFIG_KEY,
+							keycloak.tokenParsed?.sub ?? "",
+						);
+					}
+
+					set({ profile, userId: keycloak.tokenParsed?.sub });
 					set({ isInitialLoading: false });
+				}),
+				login: () => {
+					invariant(get().keycloak, "Keycloak instance not defined");
 
-					return;
-				}
+					set({ isActionsLoading: true });
 
-				const keycloak = get().keycloak;
-				invariant(keycloak, "Keycloak is initialized incorrectly");
+					get().keycloak?.login({
+						redirectUri: createRedirectUrl(
+							loginRedirectUri,
+							location.pathname,
+						).href,
+					});
 
-				const profile = await get().keycloak?.loadUserProfile();
+					set({ isActionsLoading: false });
+				},
+				register: () => {
+					invariant(get().keycloak, "Keycloak instance not defined");
 
-				if (!import.meta.env.SSR) {
-					setAuthCookie();
-					keycloak.onAuthRefreshSuccess = setAuthCookie;
+					set({ isActionsLoading: true });
 
-					window.localStorage.setItem(
-						AUTHN_SVC_SUB_CONFIG_KEY,
-						keycloak.tokenParsed?.sub ?? "",
-					);
-				}
+					get().keycloak?.register({
+						redirectUri: createRedirectUrl(
+							loginRedirectUri,
+							location.pathname,
+						).href,
+					});
 
-				set({ profile, userId: keycloak.tokenParsed?.sub });
-				set({ isInitialLoading: false });
-			}),
-			login: () => {
-				invariant(get().keycloak, "Keycloak instance not defined");
+					set({ isActionsLoading: false });
+				},
+				logout: () => {
+					invariant(get().keycloak, "Keycloak instance not defined");
 
-				set({ isActionsLoading: true });
+					deleteAuthCookie();
+					set({ isActionsLoading: true });
 
-				get().keycloak?.login({
-					redirectUri: createRedirectUrl(
-						loginRedirectUri,
-						location.pathname,
-					).href,
-				});
-
-				set({ isActionsLoading: false });
+					get().keycloak?.logout({
+						redirectUri: createRedirectUrl(logoutRedirectUri).href,
+					});
+				},
+				cleanup: once(() => {
+					clearInterval(get().refreshMapboxTokenInterval);
+				}),
 			},
-			register: () => {
-				invariant(get().keycloak, "Keycloak instance not defined");
-
-				set({ isActionsLoading: true });
-
-				get().keycloak?.register({
-					redirectUri: createRedirectUrl(
-						loginRedirectUri,
-						location.pathname,
-					).href,
-				});
-
-				set({ isActionsLoading: false });
-			},
-			logout: () => {
-				invariant(get().keycloak, "Keycloak instance not defined");
-
-				deleteAuthCookie();
-				set({ isActionsLoading: true });
-
-				get().keycloak?.logout({
-					redirectUri: createRedirectUrl(logoutRedirectUri).href,
-				});
-			},
-			cleanup: once(() => {
-				clearInterval(refreshMapboxTokenInterval);
-			}),
-		},
-	};
-});
+		};
+	},
+);
