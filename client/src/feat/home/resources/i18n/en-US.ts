d404df006383def18b381982543375bfa0ba804e
Tyrone Biggums <55116576+nmathew98@users.noreply.github.com>
2025-06-06 19:31:08 +1200

    feat: export passport applications (#189)

--- client/src/feat/home/resources/i18n/en-US.ts
+++ client/src/feat/home/resources/i18n/en-US.ts
@@ -3,4 +3,14 @@ import { CoreRouteTitle } from "core/constants/core-route.enum";
 export const resources = {
 	metaTitle: CoreRouteTitle.Home,
 	doApply: "Create a new application",
+	doExport: "Export",
+	exportAlertTitle: "Export your data",
+	exportAlertDescription: [
+		"Your data is not saved to the cloud and can be lost in the event of failure, save a local copy by exporting it.",
+		"Login or register to save your data to the cloud",
+	].join(" "),
+	exportFailedDialogTitle: "Failed to export some applications",
+	exportFailedDialogDescription: (automergeUrls: string[]) =>
+		automergeUrls.join(", "),
+	doCloseExportFailedDialog: "Ok",
 };
