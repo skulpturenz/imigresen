e04065a436d6a9b2abf9956355d7f96b22f66295
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-06 07:28:39 +0000

    fix: update copy

--- client/src/feat/home/resources/i18n/en-US.ts
+++ client/src/feat/home/resources/i18n/en-US.ts
@@ -5,8 +5,10 @@ export const resources = {
 	doApply: "Create a new application",
 	doExport: "Export",
 	exportAlertTitle: "Export your data",
-	exportAlertDescription:
-		"Your data is not saved to the cloud and can be lost in the event of failure, save a local copy by exporting it",
+	exportAlertDescription: [
+		"Your data is not saved to the cloud and can be lost in the event of failure, save a local copy by exporting it.",
+		"Login or register to save your data to the cloud",
+	].join(" "),
 	exportFailedDialogTitle: "Failed to export some applications",
 	exportFailedDialogDescription: (automergeUrls: string[]) =>
 		automergeUrls.join(", "),
