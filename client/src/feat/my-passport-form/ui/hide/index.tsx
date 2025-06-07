bcba9e25b25ff5752e4972ce49c6976541c2a46e
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-07 08:43:49 +0000

    chore: cleanup

--- client/src/feat/my-passport-form/ui/hide/index.tsx
+++ client/src/feat/my-passport-form/ui/hide/index.tsx
@@ -8,14 +8,11 @@ export interface HideProps {
 }
 
 export const Hide: Component<ParentProps<HideProps>> = props => (
-	<>
-		{cn(props.when ? "hidden" : "visible", props.class)}
-		<div
-			{...spreadProps(props)}
-			// TODO: if `props.class` is after the visibility classes, it merges incorrectly
-			// joined without a space: `gap-y-8visible` instead of `gap-y-8 visible` for example
-			class={cn(props.class, props.when ? "hidden" : "visible")}>
-			{props.children}
-		</div>
-	</>
+	<div
+		{...spreadProps(props)}
+		// TODO: if `props.class` is after the visibility classes, it merges incorrectly
+		// joined without a space: `gap-y-8visible` instead of `gap-y-8 visible` for example
+		class={cn(props.class, props.when ? "hidden" : "visible")}>
+		{props.children}
+	</div>
 );
