1d766f0491fa9ff8d1fe9403ae6204587241a203
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-07 08:42:57 +0000

    fix: issue when classes are merged

--- client/src/feat/my-passport-form/ui/hide/index.tsx
+++ client/src/feat/my-passport-form/ui/hide/index.tsx
@@ -8,9 +8,14 @@ export interface HideProps {
 }
 
 export const Hide: Component<ParentProps<HideProps>> = props => (
-	<div
-		{...spreadProps(props)}
-		class={cn(props.when ? "hidden" : "visible", props.class)}>
-		{props.children}
-	</div>
+	<>
+		{cn(props.when ? "hidden" : "visible", props.class)}
+		<div
+			{...spreadProps(props)}
+			// TODO: if `props.class` is after the visibility classes, it merges incorrectly
+			// joined without a space: `gap-y-8visible` instead of `gap-y-8 visible` for example
+			class={cn(props.class, props.when ? "hidden" : "visible")}>
+			{props.children}
+		</div>
+	</>
 );
