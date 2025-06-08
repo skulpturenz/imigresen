3063b1e023a2775110392ee2bb4ad6c578b18c81
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-08 06:23:39 +0000

    chore: remove unused form type from feat/my-passport-form

--- client/src/feat/my-passport-form/types/ui.ts
+++ client/src/feat/my-passport-form/types/ui.ts
@@ -1,7 +1,7 @@
 import type { createForm, FormStore } from "@modular-forms/solid";
+import type { MyPassportForm } from "common/types/epic/my-passport-form";
 import type { Accessor } from "solid-js";
 import type { DropdownOptions } from "./api";
-import type { MyPassportForm } from "./form";
 
 export enum Step {
 	PersonalDetails = 1,
