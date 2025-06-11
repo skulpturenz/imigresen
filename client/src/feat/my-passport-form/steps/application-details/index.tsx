86e9b317998dffb61ee1b039d3c90962802cd5cf
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-11 08:16:54 +0000

    feat: wire up select

--- client/src/feat/my-passport-form/steps/application-details/index.tsx
+++ client/src/feat/my-passport-form/steps/application-details/index.tsx
@@ -1,17 +1,17 @@
 import { useI18n } from "core/context/i18n";
 import type { resources } from "feat/my-passport-form/resources/i18n/en-us";
-import type { StepProps } from "feat/my-passport-form/types";
+import type { MyPassportForm, StepProps } from "feat/my-passport-form/types";
 import { InputGroup } from "feat/my-passport-form/ui/input-group";
 import { NextRow } from "feat/my-passport-form/ui/next-row";
 import type { Component } from "solid-js";
 import { Label } from "ui/label";
 import {
-	Select,
 	SelectContent,
 	SelectItem,
 	SelectTrigger,
 	SelectValue,
 } from "ui/select";
+import { ModularFormsSelect } from "ui/select/modular-forms-select";
 import {
 	TextField,
 	TextFieldDescription,
@@ -25,68 +25,102 @@ export const ApplicationDetails: Component<StepProps> = props => {
 	return (
 		<>
 			<div class="col-span-1">
-				<InputGroup>
-					<Label>
-						{t("form.applicationDetails.documentType.label")}
-					</Label>
+				<props.Field name="applicationDetails.documentType">
+					{(field, fieldProps) => (
+						<>
+							<InputGroup>
+								<Label>
+									{t(
+										"form.applicationDetails.documentType.label",
+									)}
+								</Label>
 
-					<Select
-						options={[
-							"Apple",
-							"Banana",
-							"Blueberry",
-							"Grapes",
-							"Pineapple",
-						]}
-						placeholder={t(
-							"form.applicationDetails.documentType.placeholder",
-						)}
-						itemComponent={props => (
-							<SelectItem item={props.item}>
-								{props.item.rawValue}
-							</SelectItem>
-						)}>
-						<SelectTrigger>
-							<SelectValue<string>>
-								{state => state.selectedOption()}
-							</SelectValue>
-						</SelectTrigger>
-						<SelectContent />
-					</Select>
-				</InputGroup>
+								<ModularFormsSelect<
+									string,
+									MyPassportForm,
+									never,
+									"input"
+								>
+									{...field}
+									{...fieldProps}
+									form={props.form}
+									options={[
+										"Apple",
+										"Banana",
+										"Blueberry",
+										"Grapes",
+										"Pineapple",
+									]}
+									placeholder={t(
+										"form.applicationDetails.documentType.placeholder",
+									)}
+									itemComponent={props => (
+										<SelectItem item={props.item}>
+											{props.item.rawValue}
+										</SelectItem>
+									)}>
+									<SelectTrigger>
+										<SelectValue<string>>
+											{state => state.selectedOption()}
+										</SelectValue>
+									</SelectTrigger>
+									<SelectContent />
+								</ModularFormsSelect>
+							</InputGroup>
+						</>
+					)}
+				</props.Field>
 			</div>
 
 			<NextRow class="col-span-1">
 				<div>
-					<InputGroup>
-						<Label>
-							{t("form.applicationDetails.requestType.label")}
-						</Label>
+					<props.Field name="applicationDetails.requestType">
+						{(field, fieldProps) => (
+							<>
+								<InputGroup>
+									<Label>
+										{t(
+											"form.applicationDetails.requestType.label",
+										)}
+									</Label>
 
-						<Select
-							options={[
-								"Apple",
-								"Banana",
-								"Blueberry",
-								"Grapes",
-								"Pineapple",
-							]}
-							placeholder={t(
-								"form.applicationDetails.requestType.placeholder",
-							)}
-							itemComponent={props => (
-								<SelectItem item={props.item}>
-									{props.item.rawValue}
-								</SelectItem>
-							)}>
-							<SelectTrigger>
-								<SelectValue<string>>
-									{state => state.selectedOption()}
-								</SelectValue>
-							</SelectTrigger>
-							<SelectContent />
-						</Select>
-					</InputGroup>
+									<ModularFormsSelect<
+										string,
+										MyPassportForm,
+										never,
+										"input"
+									>
+										{...field}
+										{...fieldProps}
+										form={props.form}
+										options={[
+											"Apple",
+											"Banana",
+											"Blueberry",
+											"Grapes",
+											"Pineapple",
+										]}
+										placeholder={t(
+											"form.applicationDetails.requestType.placeholder",
+										)}
+										itemComponent={props => (
+											<SelectItem item={props.item}>
+												{props.item.rawValue}
+											</SelectItem>
+										)}>
+										<SelectTrigger>
+											<SelectValue<string>>
+												{state =>
+													state.selectedOption()
+												}
+											</SelectValue>
+										</SelectTrigger>
+										<SelectContent />
+									</ModularFormsSelect>
+								</InputGroup>
+							</>
+						)}
+					</props.Field>
 				</div>
 			</NextRow>
 
@@ -107,7 +141,7 @@ export const ApplicationDetails: Component<StepProps> = props => {
 								<TextField
 									{...fieldProps}
 									name={field.name}
-									value={field.value || ""}
+									value={field.value ?? ""}
 									placeholder={t(
 										"form.applicationDetails.myKadNumber.placeholder",
 									)}
@@ -135,7 +169,7 @@ export const ApplicationDetails: Component<StepProps> = props => {
 								<TextField
 									{...fieldProps}
 									name={field.name}
-									value={field.value || ""}
+									value={field.value ?? ""}
 									placeholder={t(
 										"form.applicationDetails.birthDocumentNumber.placeholder",
 									)}
