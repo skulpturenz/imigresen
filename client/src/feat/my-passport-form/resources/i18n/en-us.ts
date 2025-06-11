3eae627d5e04c82ab1ac17f133af9463dc671fc8
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-11 19:37:36 +0000

    fix: update copy

--- client/src/feat/my-passport-form/resources/i18n/en-us.ts
+++ client/src/feat/my-passport-form/resources/i18n/en-us.ts
@@ -191,8 +191,8 @@ export const resources = {
 				label: [
 					"I declare that all information provided is true and correct.",
 					[
-						"I understand that if incorrect information is provided then I am liable",
-						"to be fined up to a minimum of RM 10,000 and a maximum of RM 50,000 or face imprisonment up to a minimum of",
+						"I understand that if incorrect information is provided then I may be liable",
+						"to be fined a minimum of RM 10,000 and a maximum of RM 50,000 or face imprisonment for a minimum of",
 						"1 year and a maximum of 5 years or both under the Passport Act of 1966 (renewed 1996)",
 					].join(" "),
 				],
