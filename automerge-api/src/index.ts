dbca66b2b4a58056890807103898aebff550e1ec
Tyrone Biggums <55116576+nmathew98@users.noreply.github.com>
2025-06-15 10:43:41 +1200

    feat: ship automerge-api logs (#219)

--- automerge-api/src/index.ts
+++ automerge-api/src/index.ts
@@ -3,6 +3,7 @@ import { cloudflareRateLimiter } from "@hono-rate-limiter/cloudflare";
 /* eslint-disable-next-line */
 import * as Sentry from "@sentry/cloudflare";
 import { env } from "cloudflare:workers";
+import { createConsola } from "consola";
 import { invariant } from "es-toolkit";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
@@ -17,11 +18,22 @@ import { trimTrailingSlash } from "hono/trailing-slash";
 import type { default as postgres } from "postgres";
 import { CfWebSocketNetworkAdapter } from "./cf-websocket-network-adapter";
 import { HttpHeaders, HttpMethod, StatusCode } from "./enums";
+import { ParseableReporter } from "./parseable-reporter";
 import {
 	createPgClient,
 	PgStorageAdapter,
 	warmupConnectionPool,
 } from "./pg-storage-adapter";
+
+const consola = createConsola();
+consola.addReporter(
+	new ParseableReporter(
+		env.OTEL_EXPORTER_OTLP_ENDPOINT,
+		env.OTEL_EXPORTER_OTLP_AUTH_TOKEN,
+		"imigresen",
+	),
+);
+consola.wrapAll();
 
 invariant(env.ALLOWED_ORIGINS, 'env "ALLOWED_ORIGINS" not defined');
 const ALLOWED_ORIGINS = env.ALLOWED_ORIGINS.split(",").map(origin =>
@@ -31,11 +43,14 @@ const ALLOWED_ORIGINS = env.ALLOWED_ORIGINS.split(",").map(origin =>
 interface ApiEnv {
 	Variables: {
 		pg: postgres.Sql;
+		parseableReporter: ParseableReporter;
 	};
 }
 
 const api = new Hono<ApiEnv>()
 	.get("/", async c => {
+		const parseableReporter = c.get("parseableReporter");
+
 		if (c.req.header(HttpHeaders.Upgrade) !== "websocket") {
 			return new Response("Expected Upgrade: websocket", {
 				status: StatusCode.UpgradeRequired,
@@ -57,6 +72,7 @@ const api = new Hono<ApiEnv>()
 		});
 
 		server.accept();
+		server.addEventListener("close", parseableReporter.close);
 
 		return new Response(null, {
 			status: StatusCode.SwitchingProtocols,
@@ -115,13 +131,15 @@ const app = new Hono<AppEnv>()
 			cookie: "IMIGRESEN_AUTH_COOKIE",
 		}),
 	)
-	.use((c, next) => {
-		Sentry.setUser({
-			id: c.get("jwtPayload").sub,
-		});
+	.use(
+		createMiddleware((c, next) => {
+			Sentry.setUser({
+				id: c.get("jwtPayload").sub,
+			});
 
-		return next();
-	})
+			return next();
+		}),
+	)
 	.use(
 		cors({
 			origin: origin =>
@@ -153,6 +171,18 @@ const app = new Hono<AppEnv>()
 			c.set("pg", pgClient);
 
 			await next();
+		}),
+	)
+	// want the routes to run then flush
+	// see: https://hono.dev/docs/guides/middleware#execution-order
+	.use(
+		createMiddleware(async (c, next) => {
+			await next();
+
+			const parseableReporter: ParseableReporter =
+				c.get("parseableReporter");
+
+			parseableReporter.flush();
 		}),
 	)
 	.route("/api/v1", api);
---
+++ automerge-api/src/parseable-reporter.ts
@@ -0,0 +1,77 @@
+import { env } from "cloudflare:workers";
+import type { ConsolaOptions, ConsolaReporter, LogObject } from "consola";
+import wretch from "wretch";
+import { retry } from "wretch/middlewares";
+
+export class ParseableReporter implements ConsolaReporter {
+	#queue: LogObject[] = [];
+	#interval: ReturnType<typeof setInterval>;
+
+	constructor(
+		private parseableUrl: string,
+		private credentials: string, // base64 of `username:password`
+		private stream: string,
+		private maxEntries = 250,
+		private maxRetries = 3,
+		private flushInterval = 250,
+	) {
+		this.#interval = setInterval(this.flush, this.flushInterval);
+	}
+
+	log(logObj: LogObject, _ctx: { options: ConsolaOptions }) {
+		this.#queue.push(logObj);
+
+		if (this.#queue.length >= this.maxEntries) {
+			this.flush();
+		}
+	}
+
+	close() {
+		this.flush();
+		clearInterval(this.#interval);
+	}
+
+	async flush() {
+		if (!this.#queue.length) {
+			return;
+		}
+
+		// see: https://www.parseable.com/docs/datasource/applications/javascript#send-logs-to-the-dataset
+		await wretch(this.parseableUrl)
+			.headers({
+				"Content-Type": "application/json",
+				Authorization: `Basic ${this.credentials}`,
+				"X-P-TAG-Language": "javascript",
+				"X-P-TAG-Service": "imigresen-automerge-api",
+				"X-P-TAG-Environment": env.WORKER_ENVIRONMENT,
+				"X-P-META-CF-Version-ID": env.CF_VERSION_METADATA.id,
+			})
+			// allows `wretch` to work with cf workers
+			// see: https://www.npmjs.com/package/wretch#user-content-cloudflare-workers
+			.middlewares([
+				next => async (url, opts) => {
+					const response = await next(url, opts);
+					try {
+						Reflect.get(response, "type", response);
+					} catch (_error) {
+						Object.defineProperty(response, "type", {
+							get: () => "default",
+						});
+					}
+					return response;
+				},
+				retry({
+					delayTimer: 500,
+					delayRamp: (delay, attempts) => delay * attempts,
+					maxAttempts: this.maxRetries,
+					retryOnNetworkError: false,
+					resolveWithLatestResponse: true,
+				}),
+			])
+			.post(
+				this.#queue.splice(0, this.#queue.length),
+				`/api/v1/logstream/${this.stream}`,
+			)
+			.res();
+	}
+}
