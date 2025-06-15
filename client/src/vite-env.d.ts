e49297d6c243cea2245ce36ef4b1dc615c1fddab
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-15 03:53:31 +0000

    chore: remove flipt client token check

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -11,7 +11,6 @@ interface ImportMetaEnv {
 	readonly VITE_KEYCLOAK_REALM: string;
 	readonly VITE_KEYCLOAK_CLIENT_ID: string;
 	readonly VITE_FLIPT_URL: string;
-	readonly VITE_FLIPT_CLIENT_TOKEN: string;
 	readonly VITE_FLIPT_NAMESPACE: string;
 	readonly VITE_AUTOMERGE_WSS: string;
 	readonly VITE_MAPBOX_ACCESS_TOKEN: string;
