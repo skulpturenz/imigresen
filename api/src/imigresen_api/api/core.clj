29166fb57cab924f21aceb127046481b0e4e86ad
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-17 21:26:58 +1200

    chore: build api - add workflows and fix build issues

--- api/src/imigresen_api/api/core.clj
+++ api/src/imigresen_api/api/core.clj
@@ -8,16 +8,16 @@
                                :no-doc true
                                :swagger {:info {:title "imigresen-api"}}}}])
 
-(defn health-check []
-  ["/healthcheck" ["" {:get {:handler (fn [_req] {:status (:ok status-codes)
-                                                  :body "."})
-                             :no-doc true}}]])
+(defn ping []
+  ["/ping" ["" {:get {:handler (fn [_req] {:status (:ok status-codes)
+                                           :body "."})
+                      :no-doc true}}]])
 
 (defn api-v1 []
   ["/api/v1" {:tags ["api.v1"]}
-   user/user-routes])
+   (user/user-routes)])
 
 (defn handlers []
   [(swagger-config)
-   (health-check)
+   (ping)
    (api-v1)])
