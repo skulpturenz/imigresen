cc189a91bb9d91e530385c9447391511e9b4efec
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-19 09:25:54 +0000

    chore: fix sb

--- client/src/feat/navbar/navbar.stories.tsx
+++ client/src/feat/navbar/navbar.stories.tsx
@@ -1,7 +1,7 @@
 import { AuthnProviderMock } from "core/context/authn";
 import { createAuthnContext } from "core/context/initializers";
 import { UserProviderMock } from "core/context/user";
-import { Navbar } from "core/ui/shell/navbar";
+import { Navbar } from "feat/navbar";
 import type { Meta, StoryObj as Story } from "storybook-solidjs";
 
 export default {
