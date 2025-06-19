40524b600befdda19d21a00da36fcdff4b1c0d53
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-19 09:31:46 +0000

    chore: rm old navbar files in core

--- client/src/core/ui/shell/types.ts
+++
@@ -1,6 +0,0 @@
-import type { RouteInternalProps, RouteProps } from "core/router/route";
-
-export interface NavbarItem {
-	trigger: RouteProps & RouteInternalProps;
-	children: (RouteProps & RouteInternalProps)[];
-}
