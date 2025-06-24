7a29f8320bbfc5c530f437fdbf79dffb4499536a
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-24 08:03:20 +0000

    feat: exception handling

--- api/imigresen-api/src/imigresen_api/app/core.clj
+++ api/imigresen-api/src/imigresen_api/app/core.clj
@@ -7,7 +7,7 @@
             [reitit.ring.middleware.parameters]
             [reitit.ring.middleware.muuntaja]
             [reitit.ring.coercion]
-            [reitit.ring.middleware.exception]
+            [reitit.ring.middleware.exception :as exception]
             [muuntaja.core :as m]
             [reitit.ring.middleware.multipart]
             [mount.core :as mount]
@@ -15,9 +15,11 @@
             [imigresen-common.state.db.core]
             [imigresen-common.state.flipt.core]
             [camel-snake-kebab.core :refer [->camelCase ->kebab-case]]
-            [imigresen-common.app.routes :refer [content-types with-authnz]]
+            [imigresen-common.app.routes :refer [content-types with-authnz status-codes]]
             [imigresen-common.app.middleware.cors :refer [cors-middleware]]
-            [imigresen-common.app.middleware.query-string :refer [query-string-middleware]]))
+            [imigresen-common.app.middleware.query-string :refer [query-string-middleware]]
+            [ring.util.response :refer [status]]
+            [buddy.auth :as bauth]))
 
 ;; TODO: configure `telemere` and otel
 (defn init []
@@ -34,6 +36,21 @@
        (assoc-in [:formats (:json content-types) :encoder-opts] {:encode-key-fn (comp ->camelCase name) :strip-nils true}) ;; clojure -> json
        (assoc-in [:formats (:json content-types) :decoder-opts] {:decode-key-fn (comp keyword ->kebab-case)})))) ;; json -> clojure
 
+(def exception-middleware
+  (reitit.ring.middleware.exception/create-exception-middleware
+   (merge reitit.ring.middleware.exception/default-handlers
+          {;; TODO: not matching against this for some reason
+           ::bauth/unauthorized (constantly (status (:unauthorized status-codes)))
+           ::exception/default (fn [e _request]
+                                 {:status (:internal-server-error status-codes)
+                                  :body {:message (ex-message e)
+                                         :exception (.getClass e)
+                                         :data (ex-data e)}})
+           ::exception/wrap (fn [handler e request]
+                              ;; TODO: go through logger
+                              (println "ERROR" (ex-message e) (ex-cause e) (ex-data e) (pr-str (:uri request)))
+                              (handler e request))})))
+
 (def app
   (with-authnz (ring-handler
                 (router (handlers) {:exception reitit.dev.pretty/exception
@@ -43,7 +60,7 @@
                                                         reitit.ring.middleware.parameters/parameters-middleware ;; query-params & form-params
                                                         reitit.ring.middleware.muuntaja/format-negotiate-middleware ;; content-negotiation
                                                         reitit.ring.middleware.muuntaja/format-response-middleware ;; encoding response body
-                                                        reitit.ring.middleware.exception/exception-middleware ;; exception handling
+                                                        exception-middleware ;; exception handling
                                                         reitit.ring.middleware.muuntaja/format-request-middleware ;; decoding request body
                                                         reitit.ring.coercion/coerce-response-middleware ;; coercing response body
                                                         reitit.ring.coercion/coerce-request-middleware ;; coercing request parameters
