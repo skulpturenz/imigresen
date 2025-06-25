a7e18a2237f728ff2f64f59a1478bdb46b68be78
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-25 10:32:40 +0000

    chore: nuke all untested parts - impossible to debug

--- api/imigresen-api/src/imigresen_api/api/core.clj
+++ api/imigresen-api/src/imigresen_api/api/core.clj
@@ -1,10 +1,9 @@
 (ns imigresen-api.api.core
-  (:require [imigresen-common.app.routes :refer [status-codes]]
-            [imigresen-api.api.user.core :as user]
-            [reitit.openapi :as openapi :refer [create-openapi-handler]]))
+  (:require [imigresen-common.app.routes :as imi-routes]
+            [reitit.openapi :as reitit-openapi]))
 
 (defn swagger-config []
-  ["/swagger.json" {:get {:handler (create-openapi-handler)
+  ["/swagger.json" {:get {:handler (reitit-openapi/create-openapi-handler)
                           :no-doc true
                           :openapi {:info {:title "Imigresen"}
                                     :components {:securitySchemes
@@ -12,15 +11,15 @@
                                                                   :openIdConnectUrl "https://authnz.skulpture.xyz/realms/imigresen/.well-known/openid-configuration"}}}}}}])
 
 (defn ping []
-  ["/ping" ["" {:get {:handler (fn [_req] {:status (:ok status-codes)
-                                           :body "."})
+  ["/ping" ["" {:get {:handler (constantly {:status (:ok imi-routes/status-codes) :body "."})
                       :no-doc true}}]])
 
-(defn api-v1 []
-  ["/api/v1" {:tags ["api.v1"]}
-   (user/user-routes)])
+;; (defn api-v1 []
+;;   ["/api/v1" {:tags ["api.v1"]}
+;;    (user/user-routes)])
 
 (defn handlers []
   [(swagger-config)
    (ping)
-   (api-v1)])
+   ;;(api-v1)
+   ])
--- api/imigresen-api/src/imigresen_api/app/core.clj
+++ api/imigresen-api/src/imigresen_api/app/core.clj
@@ -1,28 +1,23 @@
 (ns imigresen-api.app.core
-  (:require [reitit.ring :refer [redirect-trailing-slash-handler ring-handler router routes create-default-handler]]
+  (:require [reitit.ring :as reitit-ring]
             [reitit.swagger]
-            [reitit.swagger-ui :refer [create-swagger-ui-handler]]
+            [reitit.swagger-ui :as reitit-swagger]
             [reitit.dev.pretty]
             [reitit.coercion.spec]
             [reitit.ring.middleware.parameters]
             [reitit.ring.middleware.muuntaja]
             [reitit.ring.coercion]
-            [reitit.ring.middleware.exception :refer [create-coercion-handler]]
-            [muuntaja.core :as m]
+            [reitit.ring.middleware.exception :as reitit-exception]
             [reitit.ring.middleware.multipart]
             [mount.core :as mount]
-            [imigresen-api.api.core :refer [handlers]]
+            [imigresen-api.api.core :as imi-core]
             [imigresen-common.state.db.core]
             [imigresen-common.state.flipt.core]
-            [camel-snake-kebab.core :refer [->camelCase ->kebab-case]]
-            [imigresen-common.app.routes :refer [content-types status-codes]]
-            [imigresen-common.app.auth :refer [with-authnz]]
-            [imigresen-common.app.middleware.cors :refer [cors-middleware]]
-            [imigresen-common.app.middleware.query-string :refer [query-string-middleware]]
-            [ring.util.response :refer [status]]
-            [expound.alpha :refer [custom-printer]]))
+            [imigresen-common.app.routes :as imi-routes]
+            [imigresen-common.app.auth :as imi-auth]
+            [ring.util.response :as ring-res]
+            [expound.alpha :as expound]))
 
-;; TODO: configure `telemere` and otel
 (defn init []
   (mount/start #'imigresen-common.state.db.core/db
                #'imigresen-common.state.flipt.core/flipt))
@@ -31,28 +26,21 @@
   (mount/stop #'imigresen-common.state.db.core/db
               #'imigresen-common.state.flipt.core/flipt))
 
-(def serialize
-  (m/create
-   (-> m/default-options
-       (assoc-in [:formats (:json content-types) :encoder-opts] {:encode-key-fn (comp ->camelCase name) :strip-nils true}) ;; clojure -> json
-       (assoc-in [:formats (:json content-types) :decoder-opts] {:decode-key-fn (comp keyword ->kebab-case)})))) ;; json -> clojure
-
-(def unauthorized-exception-handler (constantly (status (:unauthorized status-codes))))
+(def unauthorized-exception-handler (constantly (ring-res/status (:unauthorized imi-routes/status-codes))))
 
 (defn default-exception-handler [ex _req]
-  {:status (:internal-server-error status-codes)
+  {:status (:internal-server-error imi-routes/status-codes)
    :body {:message (ex-message ex)
           :exception (.getClass ex)
           :data (ex-data ex)}})
 
 (defn always-exception-handler [handler ex req]
   ;; TODO: go through proper logger
-  (println "ERROR" (ex-message ex) (ex-cause ex) (ex-data ex) (pr-str (:uri req)))
   (handler ex req))
 
 (defn coercion-error-handler [status]
-  (let [printer (custom-printer {:theme :figwheel-theme, :print-specs? false})
-        handler (create-coercion-handler status)]
+  (let [printer (expound/custom-printer {:theme :figwheel-theme, :print-specs? false})
+        handler (reitit-exception/create-coercion-handler status)]
     (fn [exception request]
       (printer (-> exception ex-data :problems))
       (handler exception request))))
@@ -67,30 +55,18 @@
            :reitit.coercion/response-coercion (coercion-error-handler 500)})))
 
 (def app
-  (with-authnz (ring-handler
-                (router (handlers) {:exception reitit.dev.pretty/exception
-                                    :data {:coercion reitit.coercion.spec/coercion
-                                           :muuntaja serialize
-                                           :middleware [exception-middleware ;; exception handling
-                                                        cors-middleware ;; cors
-                                                        reitit.ring.coercion/coerce-request-middleware ;; coercing request parameters
-                                                        reitit.ring.middleware.multipart/multipart-middleware ;; multipart
-                                                        reitit.ring.middleware.parameters/parameters-middleware ;; query-params & form-params
-                                                        query-string-middleware ;; query string
-                                                        reitit.swagger/swagger-feature ;; swagger feature 
-                                                        reitit.ring.coercion/coerce-response-middleware ;; coercing response body
-                                                        reitit.ring.middleware.muuntaja/format-negotiate-middleware ;; content-negotiation
-                                                        reitit.ring.middleware.muuntaja/format-response-middleware ;; encoding response body
-                                                        reitit.ring.middleware.muuntaja/format-request-middleware ;; decoding request body
-                                                        ]}})
+  (imi-auth/with-authnz (reitit-ring/ring-handler
+                         (reitit-ring/router (imi-core/handlers) {:exception reitit.dev.pretty/exception
+                                                                  :data {:middleware [exception-middleware ;; exception handling
+                                                                                      ]}})
 
-                (routes (redirect-trailing-slash-handler)
-                        (create-swagger-ui-handler
-                         {:path "/docs"
-                          :config {:validatorUrl nil
-                                   :urls [{:name "swagger" :url "/swagger.json"}]
-                                   :urls.primaryName "swagger"
-                                   :operationsSorter "alpha"
-                                   :showRequestHeaders true
-                                   :jsonEditor true}})
-                        (create-default-handler)))))
+                         (reitit-ring/routes (reitit-ring/redirect-trailing-slash-handler)
+                                             (reitit-swagger/create-swagger-ui-handler
+                                              {:path "/docs"
+                                               :config {:validatorUrl nil
+                                                        :urls [{:name "swagger" :url "/swagger.json"}]
+                                                        :urls.primaryName "swagger"
+                                                        :operationsSorter "alpha"
+                                                        :showRequestHeaders true
+                                                        :jsonEditor true}})
+                                             (reitit-ring/create-default-handler)))))
