2cb0e6245d98347ff9e39ba79655e8c10f5cf310
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-07-09 06:59:27 +0000

    feat: GET /reference-data/im42

--- api/imigresen-api/src/imigresen_api/api/core.clj
+++ api/imigresen-api/src/imigresen_api/api/core.clj
@@ -1,9 +1,11 @@
 (ns imigresen-api.api.core
-  (:require [imigresen-api.api.v1.user :as imi-user-v1]))
+  (:require [imigresen-api.api.v1.user :as imi-user-v1]
+            [imigresen-api.api.v1.reference-data :as imi-reference-data-v1]))
 
 (defn api-v1 []
   ["/api/v1" {:tags ["api.v1"]}
-   (imi-user-v1/user-routes)])
+   (imi-user-v1/user-routes)
+   (imi-reference-data-v1/reference-data-routes)])
 
 (defn handlers []
   [(api-v1)])
--- api/imigresen-api/src/imigresen_api/api/v1/user.clj
+++ api/imigresen-api/src/imigresen_api/api/v1/user.clj
@@ -7,65 +7,65 @@
             [ring.util.response :as ring-res]))
 
 (defn user-routes []
-  ["" {:tags ["user.v1"]}
-   ["/user" {:get {:summary "Get user details by email"
-                   :handler (fn [{:keys [parameters] :as _req}]
-                              (-> (ring-res/response (imi-user/get-user-by-email (get-in parameters [:query :email])))
-                                  (ring-res/status (:ok imi-routes/status-codes))))
-                   :parameters {:query {:email ::imi-user-spec/email}}
-                   :responses {(:ok imi-routes/status-codes) {:description "Ok"
-                                                              :body imi-user-spec/user}
-                               (:bad-request imi-routes/status-codes) {:description "Bad request"}
-                               (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
-                               (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
-                   :middleware [imi-auth/protect]}
-             :post {:summary "Create a new user"
+  ["/user" {:tags ["user.v1"]}
+   ["" {:get {:summary "Get user details by email"
+              :handler (fn [{:keys [parameters] :as _req}]
+                         (-> (ring-res/response (imi-user/get-user-by-email (get-in parameters [:query :email])))
+                             (ring-res/status (:ok imi-routes/status-codes))))
+              :parameters {:query {:email ::imi-user-spec/email}}
+              :responses {(:ok imi-routes/status-codes) {:description "Ok"
+                                                         :body imi-user-spec/user}
+                          (:bad-request imi-routes/status-codes) {:description "Bad request"}
+                          (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
+                          (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
+              :middleware [imi-auth/protect]}
+        :post {:summary "Create a new user"
+               :handler (fn [{:keys [parameters] :as _req}]
+                          (-> (ring-res/response (imi-user/create-user-by-email! (:body parameters)))
+                              (ring-res/status (:created imi-routes/status-codes))))
+               :parameters {:body (ds/spec {:name ::post-user
+                                            :spec {:email ::imi-user-spec/email
+                                                   :first-name ::imi-user-spec/name
+                                                   :last-name ::imi-user-spec/name
+                                                   :password ::imi-user-spec/password}})}
+               :responses {(:created imi-routes/status-codes) {:description "Created"
+                                                               :body imi-user-spec/user}
+                           (:bad-request imi-routes/status-codes) {:description "Bad request"}
+                           (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}}}]
+   ["/:uuid" {:get {:summary "Get user details by UUID"
                     :handler (fn [{:keys [parameters] :as _req}]
-                               (-> (ring-res/response (imi-user/create-user-by-email! (:body parameters)))
-                                   (ring-res/status (:created imi-routes/status-codes))))
-                    :parameters {:body (ds/spec {:name ::post-user
+                               (-> (ring-res/response (imi-user/get-user-by-uuid (get-in parameters [:path :uuid])))
+                                   (ring-res/status (:ok imi-routes/status-codes))))
+                    :parameters {:path {:uuid ::imi-user-spec/uuid}}
+                    :responses {(:ok imi-routes/status-codes) {:description "Ok"
+                                                               :body imi-user-spec/user}
+                                (:bad-request imi-routes/status-codes) {:description "Bad request"}
+                                (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
+                                (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
+                    :middleware [imi-auth/protect]}
+              :put {:summary "Update user by UUID"
+                    :handler (fn [{:keys [identity parameters] :as _req}]
+                               (imi-user/update-user-by-uuid! identity (get-in parameters [:path :uuid]) (:body parameters))
+                               (-> (ring-res/response nil)
+                                   (ring-res/status (:no-content imi-routes/status-codes))))
+                    :parameters {:path {:uuid ::imi-user-spec/uuid}
+                                 :body (ds/spec {:name ::put-user
                                                  :spec {:email ::imi-user-spec/email
                                                         :first-name ::imi-user-spec/name
-                                                        :last-name ::imi-user-spec/name
-                                                        :password ::imi-user-spec/password}})}
-                    :responses {(:created imi-routes/status-codes) {:description "Created"
-                                                                    :body imi-user-spec/user}
+                                                        :last-name ::imi-user-spec/name}})}
+                    :responses {(:no-content imi-routes/status-codes) {:description "No content"}
                                 (:bad-request imi-routes/status-codes) {:description "Bad request"}
-                                (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}}}]
-   ["/user/:uuid" {:get {:summary "Get user details by UUID"
-                         :handler (fn [{:keys [parameters] :as _req}]
-                                    (-> (ring-res/response (imi-user/get-user-by-uuid (get-in parameters [:path :uuid])))
-                                        (ring-res/status (:ok imi-routes/status-codes))))
-                         :parameters {:path {:uuid ::imi-user-spec/uuid}}
-                         :responses {(:ok imi-routes/status-codes) {:description "Ok"
-                                                                    :body imi-user-spec/user}
-                                     (:bad-request imi-routes/status-codes) {:description "Bad request"}
-                                     (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
-                                     (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
-                         :middleware [imi-auth/protect]}
-                   :put {:summary "Update user by UUID"
-                         :handler (fn [{:keys [identity parameters] :as _req}]
-                                    (imi-user/update-user-by-uuid! identity (get-in parameters [:path :uuid]) (:body parameters))
-                                    (-> (ring-res/response nil)
-                                        (ring-res/status (:no-content imi-routes/status-codes))))
-                         :parameters {:path {:uuid ::imi-user-spec/uuid}
-                                      :body (ds/spec {:name ::put-user
-                                                      :spec {:email ::imi-user-spec/email
-                                                             :first-name ::imi-user-spec/name
-                                                             :last-name ::imi-user-spec/name}})}
-                         :responses {(:no-content imi-routes/status-codes) {:description "No content"}
-                                     (:bad-request imi-routes/status-codes) {:description "Bad request"}
-                                     (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
-                                     (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
-                         :middleware [imi-auth/protect]}
-                   :delete {:summary "Delete user by UUID"
-                            :handler (fn [{:keys [identity parameters] :as _req}]
-                                       (imi-user/delete-user-by-uuid! identity (get-in parameters [:path :uuid]))
-                                       (-> (ring-res/response nil)
-                                           (ring-res/status (:no-content imi-routes/status-codes))))
-                            :parameters {:path {:uuid ::imi-user-spec/uuid}}
-                            :responses {(:no-content imi-routes/status-codes) {:description "No content"}
-                                        (:bad-request imi-routes/status-codes) {:description "Bad request"}
-                                        (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
-                                        (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
-                            :middleware [imi-auth/protect]}}]])
+                                (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
+                                (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
+                    :middleware [imi-auth/protect]}
+              :delete {:summary "Delete user by UUID"
+                       :handler (fn [{:keys [identity parameters] :as _req}]
+                                  (imi-user/delete-user-by-uuid! identity (get-in parameters [:path :uuid]))
+                                  (-> (ring-res/response nil)
+                                      (ring-res/status (:no-content imi-routes/status-codes))))
+                       :parameters {:path {:uuid ::imi-user-spec/uuid}}
+                       :responses {(:no-content imi-routes/status-codes) {:description "No content"}
+                                   (:bad-request imi-routes/status-codes) {:description "Bad request"}
+                                   (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
+                                   (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
+                       :middleware [imi-auth/protect]}}]])
