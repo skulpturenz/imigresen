664dca236c03e98f18ad78d385b0f5d574308a86
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-07-24 06:51:09 +0000

    fix: conflicting route

--- api/imigresen-api/src/imigresen_api/api/v1/im42.clj
+++ api/imigresen-api/src/imigresen_api/api/v1/im42.clj
@@ -48,22 +48,22 @@
                              :middleware [imi-auth/protect]}}]]
    ;; draft forms are stored in automerge repo until submitted
    ;; so all we store is an automerge url no data
-   ["/draft/user/:user-uuid" {:get {:summary "Get draft IM42 forms"
-                                    :description "Returns a sorted list of IM42 UUIDs to automerge urls, sort: desc time registered"
-                                    :handler (fn [{:keys [parameters] :as _req}]
-                                               (-> (imi-im42/get-draft-im42-forms-by-user-uuid
-                                                    (truss/have imi-user/active-by-uuid?
-                                                                (get-in parameters [:path :user-uuid])
-                                                                :data {:type :not-found}))
-                                                   (ring-res/response)
-                                                   (ring-res/status (:ok imi-routes/status-codes))))
-                                    :parameters {:path {:user-uuid ::imi-im42-spec/uuid}}
-                                    :responses {(:ok imi-routes/status-codes) {:description "Ok"
-                                                                               :body vector?}
-                                                (:not-found imi-routes/status-codes) {:description "Not found"}
-                                                (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
-                                                (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
-                                    :middleware [imi-auth/protect]}}]
+   ["/status/draft/user/:user-uuid" {:get {:summary "Get draft IM42 forms"
+                                           :description "Returns a sorted list of IM42 UUIDs to automerge urls, sort: desc time registered"
+                                           :handler (fn [{:keys [parameters] :as _req}]
+                                                      (-> (imi-im42/get-draft-im42-forms-by-user-uuid
+                                                           (truss/have imi-user/active-by-uuid?
+                                                                       (get-in parameters [:path :user-uuid])
+                                                                       :data {:type :not-found}))
+                                                          (ring-res/response)
+                                                          (ring-res/status (:ok imi-routes/status-codes))))
+                                           :parameters {:path {:user-uuid ::imi-im42-spec/uuid}}
+                                           :responses {(:ok imi-routes/status-codes) {:description "Ok"
+                                                                                      :body vector?}
+                                                       (:not-found imi-routes/status-codes) {:description "Not found"}
+                                                       (:unauthorized imi-routes/status-codes) {:description "Unauthorized"}
+                                                       (:internal-server-error imi-routes/status-codes) {:description "Internal server error"}}
+                                           :middleware [imi-auth/protect]}}]
    ["/:uuid/user/:user-uuid" {:put {:summary "Update a registered IM42 form"
                                     :handler (fn [{:keys [identity parameters] :as _req}]
                                                (truss/have
