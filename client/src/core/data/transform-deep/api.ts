5045a44cc248b2dbfeae61a83092be6956377294
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-08-08 22:10:31 +0000

    fix: date conformer predicate

--- client/src/core/data/transform-deep/api.ts
+++ client/src/core/data/transform-deep/api.ts
@@ -2,7 +2,8 @@ import { createConformer, transformDeep } from "./transform-deep";
 
 const conformSerializedDate = createConformer(
 	value => new Date(value as string),
-	value => typeof value === "string" && !Number.isNaN(new Date(value)),
+	value =>
+		typeof value === "string" && !Number.isNaN(new Date(value).getTime()),
 );
 
 export const normalizeResponse = <U, T = any>(value: T) =>
