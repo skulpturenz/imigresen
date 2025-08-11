7f4985971b999e7ec38bcf59fcf914f05939a618
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-08-11 08:56:31 +0000

    chore: fix build

--- client/src/feat/my-passport-form/spec/application-details.ts
+++ client/src/feat/my-passport-form/spec/application-details.ts
@@ -21,7 +21,7 @@ export const applicationDetails = object({
 				"Reference data not defined in yup context",
 			);
 
-			if (!context.dropdownOptions?.documentTypeOptions[value]) {
+			if (!context.dropdownOptions()?.documentTypeOptions[value]) {
 				return testContext.createError({
 					message: t("form.errors.invalidOption"),
 				});
