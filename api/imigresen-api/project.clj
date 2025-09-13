b3a9189164da5f57bd514b8a2fc5db99e5ac932c
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-09-13 00:06:17 +0000

    chore: simplify checking out deps during dev process, migrate to using lein-checkout-deps

--- api/imigresen-api/project.clj
+++ api/imigresen-api/project.clj
@@ -23,74 +23,26 @@
                  [ring/ring-jetty-adapter "1.14.2"]
                  [nrepl/nrepl "1.4.0"]
                  [cider/cider-nrepl "0.57.0"]
-                 ;; comment when dev - use checkout
                  ;; lein monolith link imigresen/common
-                 [imigresen/common "SNAPSHOT"] ;;
-                 ]
+                 [imigresen/common "SNAPSHOT"]]
   :resource-paths ["resources"]
   :main ^:skip-aot imigresen-api.app.server
   :target-path "target/%s"
   :profiles {:dev {:env {:java-env "development"
                          :taoensso-telemere-rt-min-level ":debug"}
                    :dependencies [[ring/ring-devel "1.14.1"]
-                                  ;; imigresen-common deps for checkout
-                                  [keycloak-clojure/keycloak-clojure "1.31.5"]
-                                  [com.github.seancorfield/honeysql "2.7.1310"]
-                                  [com.github.seancorfield/next.jdbc "1.3.1048"]
-                                  [org.postgresql/postgresql "42.7.7"]
-                                  [com.zaxxer/HikariCP "6.3.0"]
-                                  [environ "1.2.0"]
-                                  [metosin/reitit "0.9.1"]
-                                  [metosin/ring-swagger-ui "5.20.0"]
-                                  [metosin/muuntaja "0.6.11"]
-                                  [mount "0.1.23"]
-                                  [buddy/buddy-auth "3.0.323"]
-                                  [org.clojure/core.match "1.1.0"]
-                                  [migratus "1.6.4"]
-                                  [org.slf4j/slf4j-log4j12 "2.0.17"]
-                                  [com.taoensso/telemere "1.0.1"]
-                                  [http-kit "2.8.0"]
-                                  [danlentz/clj-uuid "0.2.0"]
-                                  [clojure.java-time "1.4.3"]
-                                  [org.threeten/threeten-extra "1.8.0"]
-                                  [camel-snake-kebab "0.4.3"]
-                                  [jumblerg/ring-cors "3.0.0"]
-                                  [com.taoensso/truss "2.1.0"]
-                                  [io.opentelemetry/opentelemetry-api "1.50.0"]
-                                  [org.clojure/tools.logging "1.3.0"]
-                                  [io.sentry/sentry-clj "7.22.227"]
-                                  [metosin/spec-tools "0.10.7"]
-                                  [metosin/jsonista "0.3.13"]
-                                  [io.randomseed/phone-number "8.13.6-3"]
                                   [io.github.tonsky/clj-reload "0.9.8"]
-                                  [watchtower "0.1.1"]
-                                  ;; AWS dependencies
-                                  [amazonica "0.3.168" :exclusions [com.amazonaws/aws-java-sdk
-                                                                    com.amazonaws/amazon-kinesis-client
-                                                                    com.amazonaws/dynamodb-lock-client]]
-                                  [com.amazonaws/aws-java-sdk-s3 "1.12.788"]
-                                  [com.amazonaws/aws-java-sdk-core "1.12.788"]
-                                  ;; imigresen-common test deps for checkout
-                                  ;; hmr fails otherwise
-                                  [clj-test-containers/clj-test-containers "0.7.4"]
-                                  [org.testcontainers/postgresql "1.21.2"]
-                                  ;; not required for dev, its only used in tests but
-                                  ;; hmr throws without it
-                                  [ring/ring-mock "0.6.1"]]}
+                                  [watchtower "0.1.1"]]}
              :uberjar {:env {:java-env "production"
                              :timbre-level "ERROR"
                              :log-level "ERROR"}
                        :aot [imigresen-api.app.server]
                        ;; https://cljdoc.org/d/com.taoensso/telemere/1.0.1/api/taoensso.telemere.tools-logging#tools-logging-%3Etelemere!
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true -Dclojure.tools.logging.to-telemere=true"]
-                       ;; checkout for dev
-                       :dependencies [[imigresen/common "SNAPSHOT"]]}
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true -Dclojure.tools.logging.to-telemere=true"]}
              :test {:env {:timbre-level "ERROR"
                           :log-level "ERROR"
                           :java-env "test"}
-                    :dependencies [;; checkout for dev
-                                   [imigresen/common "SNAPSHOT"]
-                                   [org.clojure/data.json "2.5.1"]
+                    :dependencies [[org.clojure/data.json "2.5.1"]
                                    [ring/ring-mock "0.6.1"]]}}
   :test-paths ["src"]
   :plugins [[lein-environ "LATEST"]
@@ -98,14 +50,15 @@
             [migratus-lein "0.7.3"]
             [lein-ancient "LATEST"]
             [dev.weavejester/lein-cljfmt "LATEST"]
-            [lein-monolith "LATEST"]]
+            [lein-monolith "LATEST"]
+            [lein-checkout-deps "1.0.0"]]
   ;; uncomment to seed database
   ;; :migratus {:migration-dir "seeds"}
-  :aliases {"dev" ["run"]
+  :aliases {"dev" ["do" "deps," "run"]
             "build" ["uberjar"]
             "build.watch" ["auto" "uberjar"]
-            "test" ["test"]
-            "test.watch" ["auto" "test"]
+            "test" ["do", "deps," "test"]
+            "test.watch" ["do" "deps," "auto" "test"]
             "repl" ["repl"]}
   :test-selectors {:default (complement :integration)
                    :unit (fn
