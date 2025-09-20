e5df23b17895ad5cebd6dc633da2748a46aaff21
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-09-20 22:06:31 +0000

    chore(#462): swagger refresh auth token

--- api/imigresen-api/resources/swagger-ui/swagger-initializer.js
+++ api/imigresen-api/resources/swagger-ui/swagger-initializer.js
@@ -1,7 +1,12 @@
 window.onload = function () {
     //<editor-fold desc="Changeable Configuration Block">
 
-    let token = "";
+    let accessToken = "";
+    let refreshToken = "";
+    let accessExpires = -1;
+    let refreshIntervalId;
+
+    const toMs = (seconds) => seconds * Math.pow(10, 3);
 
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
@@ -13,8 +18,8 @@ window.onload = function () {
             SwaggerUIStandalonePreset
         ],
         requestInterceptor: request => {
-            if (token) {
-                request.headers['Authorization'] = `Bearer ${token}`;
+            if (accessToken) {
+                request.headers['Authorization'] = `Bearer ${accessToken}`;
             }
 
             return request;
@@ -25,7 +30,38 @@ window.onload = function () {
             }
 
             const tokenParsed = response.body;
-            token = tokenParsed.access_token;
+            accessToken = tokenParsed.access_token;
+            refreshToken = tokenParsed.refresh_token;
+            accessExpires = tokenParsed.expires_in;
+
+            if (refreshIntervalId) {
+                clearInterval(refreshIntervalId);
+            }
+
+            const getNewAccessToken = async () => {
+                const res = await fetch('https://authnz.skulpture.xyz/realms/imigresen/protocol/openid-connect/token', {
+                    method: 'POST',
+                    body: new URLSearchParams({
+                        client_id: 'swagger',
+                        grant_type: 'refresh_token',
+                        refresh_token: refreshToken
+                    }),
+                });
+
+                const result = await res.json();
+
+                console.log('result', result);
+                if (res.ok) {
+                    accessToken = result.access_token;
+                    refreshToken = result.refresh_token;
+                    accessExpires = result.expires_in;
+                }
+            }
+
+            refreshIntervalId = setInterval(() => {
+                console.log('Here!! getNewAccessToken');
+                getNewAccessToken();
+            }, toMs(accessExpires - 5));
 
             return response;
         },
