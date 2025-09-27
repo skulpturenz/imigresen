0fc39ff0ac7f3d561cedfd5f25c900b29af83a14
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-09-27 22:38:27 +0000

    fix: update exception handlers

--- api/imigresen-api/src/imigresen_api/app/core.clj
+++ api/imigresen-api/src/imigresen_api/app/core.clj
@@ -46,13 +46,20 @@
                              (.write (str body))
                              (.close)))})
 
-(def unauthorized-exception-handler (constantly (ring-res/status (:unauthorized imi-routes/status-codes))))
+(defn unauthorized-exception-handler [ex _req]
+  {:status (:unauthorized imi-routes/status-codes)
+   :body {:message (or (get-in (ex-data ex) [:data :message])
+                       "Unauthorized")}})
 
-(def not-found-exception-handler (constantly (ring-res/status (:not-found imi-routes/status-codes))))
+(defn not-found-exception-handler [ex _req]
+  {:status (:not-found imi-routes/status-codes)
+   :body {:message (or (get-in (ex-data ex) [:data :message])
+                       "Not found")}})
 
 (defn bad-request-exception-handler [ex _req]
   {:status (:bad-request imi-routes/status-codes)
-   :body {:message (get-in (ex-data ex) [:data :message])}})
+   :body {:message (or (get-in (ex-data ex) [:data :message])
+                       "Bad request")}})
 
 (defn default-exception-handler [ex _req]
   {:status (:internal-server-error imi-routes/status-codes)
