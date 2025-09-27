e5f88208c20474603f15b503cd869cbc89b88a9d
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-09-27 12:35:46 +0000

    chore: delay testcontainer start

--- api/skulpture-eventing/src/skulpture_eventing/test_utils/db/mock.clj
+++ api/skulpture-eventing/src/skulpture_eventing/test_utils/db/mock.clj
@@ -6,13 +6,13 @@
   (:import [org.testcontainers.containers PostgreSQLContainer]))
 
 ;; https://cljdoc.org/d/clj-test-containers/clj-test-containers/0.7.4/doc/readme
-(def container (-> (tc/init {:container (PostgreSQLContainer. "postgres:16-alpine")
-                             :exposed-ports [5432]})
-                   (tc/start!)))
+(def container (delay (-> (tc/init {:container (PostgreSQLContainer. "postgres:16-alpine")
+                                    :exposed-ports [5432]})
+                          (tc/start!))))
 
 (defn- start-mock []
-  (let [host (:host container)
-        port (.getMappedPort (:container container) 5432)
+  (let [host (:host @container)
+        port (.getMappedPort (:container @container) 5432)
         user "test"
         password "test"
         database "test"]
@@ -21,7 +21,7 @@
 
 (defn- stop-mock []
   (db-mock/stop)
-  (tc/stop! container))
+  (tc/stop! @container))
 
 (mount/defstate db
   :start (start-mock)
