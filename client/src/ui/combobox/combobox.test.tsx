c4b9a0078c7a795c2400c87293e055d649da5300
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-10-11 04:53:52 +0000

    test: add TODO unit tests

---
+++ client/src/ui/combobox/combobox.test.tsx
@@ -0,0 +1,31 @@
+import { describe, it } from "vitest";
+
+describe("<Combobox />", () => {
+	it.todo("emits an input event when new option is selected");
+
+	it.todo("emits an input event when custom option is selected");
+
+	it.todo("filters options based on input value");
+
+	it.todo("filters options when a custom filter is specified");
+
+	it.todo("creates only one custom option");
+
+	it.todo("does not create a custom option if it already exists");
+
+	it.todo("can be focused");
+
+	it.todo("can be blurred");
+
+	it.todo("can be clicked");
+
+	it.todo("resets input value to selected option when it loses focus");
+
+	it.todo("clears filters when it loses focus");
+
+	it.todo("can be controlled");
+
+	it.todo("throws an error if its content is not a render function");
+
+	it.todo("can be cleared");
+});
---
+++ client/src/ui/combobox/searchbox.test.tsx
@@ -0,0 +1,21 @@
+import { describe, it } from "vitest";
+
+describe("<Searchbox />", () => {
+	it.todo("can be controlled");
+
+	it.todo("emits an event when input value is changed");
+
+	it.todo("allows search strings which are not valid options");
+
+	it.todo("can be focused");
+
+	it.todo("can be blurred");
+
+	it.todo("can be clicked");
+
+	it.todo("does not reset value when focus is lost");
+
+	it.todo("throws an error if its content is not a valid JSX element");
+
+	it.todo("can be cleared");
+});
