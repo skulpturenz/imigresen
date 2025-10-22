68a0092fbe08aa70f0a7244661551956acb64be2
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-10-22 05:33:15 +0000

    test: add TODO unit tests

---
+++ client/src/ui/date-picker/date-picker.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it } from "vitest";
+
+describe.sequential("<DatePicker />", () => {
+	it.todo("emits an input event when a new date is selected");
+
+	it.todo("ref can be focused");
+
+	it.todo("ref can be clicked");
+
+	it.todo("ref can be blurred");
+});
