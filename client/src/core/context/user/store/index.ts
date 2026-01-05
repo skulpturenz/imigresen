c122f56259b61b53e2c26f0102084c135a1765fd
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2026-01-05 09:30:15 +0000

    refactor: user svc - handle errors better, refactor how we handle state

--- client/src/core/context/user/store/index.ts
+++ client/src/core/context/user/store/index.ts
@@ -1,9 +1,8 @@
-import { useQuery } from "@tanstack/solid-query";
+import { QueryClient } from "@tanstack/solid-query";
 import { queryKeys } from "core/constants/query-keys";
 import { normalizeResponse } from "core/data/transform-deep";
 import { invariant, once } from "es-toolkit";
 import type { KeycloakProfile } from "keycloak-js";
-import { createEffect } from "solid-js";
 import { createWithSignal } from "solid-zustand";
 import { default as wretch } from "wretch";
 
@@ -30,12 +29,19 @@ export interface UserPersonalDetails {
 	nationalityCountryCode: string;
 }
 
+export interface UserSvcProps {
+	queryClient: QueryClient;
+}
+
 export interface UserSvc {
 	isInitialLoading: boolean;
 	profile?: UserProfile | null;
 	syncComplete?: boolean;
 	actions: {
-		init: (token?: string, profile?: KeycloakProfile | null) => void;
+		init: (
+			token?: string,
+			profile?: KeycloakProfile | null,
+		) => Promise<void>;
 		completeSync: (token?: string) => void;
 	};
 }
@@ -51,110 +57,94 @@ const personalDetailsApi = wretch(
 	`${import.meta.env.VITE_API_BASE_URL}/personal-details`,
 );
 
-export const useStore = createWithSignal<UserSvc>((set, get) => {
-	return {
-		isInitialLoading: true,
-		profile: null,
-		actions: {
-			init: once(
-				async (token?: string, profile?: KeycloakProfile | null) => {
-					if (!token || !profile) {
-						set({ isInitialLoading: false });
+export const useStore = (props: UserSvcProps) => {
+	const store = createWithSignal<UserSvc>((set, get) => {
+		return {
+			isInitialLoading: true,
+			profile: null,
+			actions: {
+				init: once(
+					async (
+						token?: string,
+						profile?: KeycloakProfile | null,
+					) => {
+						if (!token || !profile) {
+							set({ isInitialLoading: false });
 
-						return;
-					}
-
-					invariant(profile.email, "No email for keycloak profile");
-
-					const searchParams = new URLSearchParams({
-						email: profile.email,
-					});
-
-					const qUser = useQuery(() => ({
-						queryKey: queryKeys.getUserDetails(token),
-						queryFn: () =>
-							userApi
-								.auth(`Bearer ${token}`)
-								.get(`?${searchParams.toString()}`)
-								.json(normalizeResponse<UserProfile>),
-					}));
-
-					const qPersonalDetails = useQuery(() => ({
-						queryKey: queryKeys.getPersonalDetails(token),
-						queryFn: () =>
-							personalDetailsApi
-								.auth(`Bearer ${token}`)
-								.get(`/user/${qUser.data?.uuid}`)
-								.notFound(() => null)
-								.json(
-									normalizeResponse<UserPersonalDetails | null>,
-								),
-						enabled: Boolean(qUser.data),
-					}));
-
-					const qIm42Config = useQuery(() => ({
-						queryKey: queryKeys.getPersonalDetails(token),
-						queryFn: () =>
-							userApi
-								.auth(`Bearer ${token}`)
-								.get(`/${qUser.data?.uuid}/config/im42`)
-								.json(normalizeResponse<IM42Config>),
-						enabled: Boolean(qUser.data),
-					}));
-
-					createEffect(() => {
-						if (
-							qUser.isLoading ||
-							qPersonalDetails.isLoading ||
-							!qUser.data
-						) {
 							return;
 						}
+
+						invariant(
+							profile.email,
+							"No email for keycloak profile",
+						);
+
+						const searchParams = new URLSearchParams({
+							email: profile.email,
+						});
+
+						const userDetails = await props.queryClient.fetchQuery({
+							queryKey: queryKeys.getUserDetails(token),
+							queryFn: () =>
+								userApi
+									.auth(`Bearer ${token}`)
+									.get(`?${searchParams.toString()}`)
+									.json(normalizeResponse<UserProfile>),
+						});
+
+						const personalDetails =
+							await props.queryClient.fetchQuery({
+								queryKey: queryKeys.getPersonalDetails(token),
+								queryFn: () =>
+									personalDetailsApi
+										.auth(`Bearer ${token}`)
+										.get(`/user/${userDetails.uuid}`)
+										.notFound(() => null)
+										.json(
+											normalizeResponse<UserPersonalDetails | null>,
+										),
+							});
 
 						set({
 							profile: {
-								...qUser.data,
+								...userDetails,
 								phoneNumber:
-									qPersonalDetails.data?.mobileNumber || "",
+									personalDetails?.mobileNumber || "",
 							},
 						});
-					});
 
-					createEffect(() => {
-						if (!qUser.data || !qIm42Config.data) {
-							return;
-						}
+						const im42Config = await props.queryClient.fetchQuery({
+							queryKey: queryKeys.getPersonalDetails(token),
+							queryFn: () =>
+								userApi
+									.auth(`Bearer ${token}`)
+									.get(`/${userDetails.uuid}/config/im42`)
+									.json(normalizeResponse<IM42Config>)
+									.catch(() => Object.create(null)),
+						});
 
 						set({
-							syncComplete: Boolean(qIm42Config.data.syncedAt),
+							syncComplete: Boolean(im42Config.syncedAt),
 						});
-					});
-
-					createEffect(() => {
-						if (
-							qUser.isLoading ||
-							qPersonalDetails.isLoading ||
-							qIm42Config.isLoading
-						) {
-							return;
-						}
 
 						set({ isInitialLoading: false });
-					});
+					},
+				),
+				completeSync: (token?: string) => {
+					const profile = get().profile;
+					if (!token || !profile?.uuid) {
+						return;
+					}
+
+					im42Api
+						.auth(`Bearer ${token}`)
+						.put(`/user/${profile.uuid}/config/synced`);
+
+					set({ syncComplete: true });
 				},
-			),
-			completeSync: (token?: string) => {
-				const profile = get().profile;
-				if (!token || !profile?.uuid) {
-					return;
-				}
-
-				im42Api
-					.auth(`Bearer ${token}`)
-					.put(`/user/${profile.uuid}/config/synced`);
-
-				set({ syncComplete: true });
 			},
-		},
-	};
-});
+		};
+	});
+
+	return store(); // returns an accessor which IS reactive
+};
