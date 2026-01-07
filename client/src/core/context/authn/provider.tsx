0c25ac803e9c2afa8119f3ec488c60e6192a7ec5
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2026-01-07 06:26:41 +0000

    feat: add degradation dialog

--- client/src/core/context/authn/provider.tsx
+++ client/src/core/context/authn/provider.tsx
@@ -9,6 +9,16 @@ import {
 	type Component,
 	type ParentProps,
 } from "solid-js";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "ui/alert-dialog";
+import { resources } from "./resources";
 import { useStore, type AuthnSvc } from "./store";
 
 export const AuthnContext =
@@ -34,6 +44,10 @@ const AuthnProviderWithoutErrorBoundary: Component<ParentProps> = props => {
 			<Track fn={onMount} />
 			<AuthnContext.Provider value={value}>
 				<Show when={!value().isInitialLoading}>{props.children}</Show>
+
+				<Show when={value().isInitialError}>
+					<DegradationDialog />
+				</Show>
 			</AuthnContext.Provider>
 		</>
 	);
@@ -46,3 +60,25 @@ export const AuthnProvider: Component<ParentProps> = props => (
 		</AuthnProviderWithoutErrorBoundary>
 	</ErrorBoundary>
 );
+
+const DegradationDialog: Component = _props => {
+	return (
+		<AlertDialog defaultOpen>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>
+						{resources.degradationDialog.title}
+					</AlertDialogTitle>
+					<AlertDialogDescription class="whitespace-pre-line">
+						{resources.degradationDialog.description}
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogAction>
+						{resources.degradationDialog.doOk}
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+};
