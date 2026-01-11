73c25b4478af68994ec8e10c6ce831a7d60489ef
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2026-01-11 00:15:34 +0000

    refactor: generate model path

--- client/src/core/models/index.ts
+++ client/src/core/models/index.ts
@@ -1,3 +1,14 @@
+const v = (publicPath: string, version: Date) => {
+	const searchParams = new URLSearchParams({
+		version: `${version.getTime()}`,
+	});
+
+	return `${publicPath}?${searchParams.toString()}`;
+};
+
 export const models = {
-	signverodAutomlEdge: `/models/signverod-automl-edge/model.json?version=20260111`,
+	signverodAutomlEdge: v(
+		"/models/signverod-automl-edge/model.json",
+		new Date(2026, 0, 11, 0, 0, 0, 0),
+	),
 };
