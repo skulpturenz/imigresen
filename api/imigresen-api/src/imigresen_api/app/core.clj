0c36f861049d06a4b2a3259192fdd57d1994ff96
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2026-02-12 08:05:13 +0000

    refactor: relax location header check, some? just checks if its nil

--- api/imigresen-api/src/imigresen_api/app/core.clj
+++ api/imigresen-api/src/imigresen_api/app/core.clj
@@ -136,7 +136,7 @@
   (fn [req]
     (let [res (handler req)]
       (if (and (imi-env/preview? (imi-env/current-env))
-               (some? (get-in res [:headers "Location"]))
+               (not (empty? (get-in res [:headers "Location"])))
                (not (empty? (imi-env/env :buang-deployment-path string? "")))
                (not (str/includes? (get-in res [:headers "Location"]) (imi-env/env :buang-deployment-path string? ""))))
         (assoc-in res [:headers "Location"] (str (imi-env/env :buang-deployment-path string? "") (get-in res [:headers "Location"])))
