854114922dde32c59367e494d9713d2acb66e0aa
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2026-02-12 09:00:48 +0000

    chore: fix formatting

--- api/imigresen-api/src/imigresen_api/app/core.clj
+++ api/imigresen-api/src/imigresen_api/app/core.clj
@@ -2,6 +2,7 @@
   (:require [camel-snake-kebab.core :as csk]
             [clj-commons.format.exceptions :as pexceptions]
             [clojure.java.io :as io]
+            [clojure.pprint]
             [clojure.string :as str]
             [expound.alpha :as expound]
             [imigresen-api.api.core :as imi-core]
@@ -30,8 +31,7 @@
             [ring.logger :as logger]
             [ring.util.response :as ring-res]
             [sentry-clj.core :as sentry]
-            [taoensso.telemere :as tel]
-            [clojure.pprint])
+            [taoensso.telemere :as tel])
   (:import (java.io Writer)
            (java.util UUID)))
 
